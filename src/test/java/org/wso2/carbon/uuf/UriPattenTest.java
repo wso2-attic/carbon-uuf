fcd336a62486782e94844ff87893894c0f046a37
manuranga <manu@wso2.com>
2016-03-05 15:11:09 +0530

    adding tests for invariants

--- src/test/java/org/wso2/carbon/uuf/UriPattenTest.java
+++ src/test/java/org/wso2/carbon/uuf/UriPattenTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.wso2.carbon.uuf.core.UriPatten;
 
+import static java.lang.Integer.signum;
+
 public class UriPattenTest {
 
     @DataProvider
@@ -18,6 +20,8 @@ public class UriPattenTest {
                 {"/a/b/", "/{a}/{b}/"},
                 {"/{a}/b", "/{a}/{b}"},
                 {"/a/{b}", "/{a}/{b}"},
+                {"/ab", "/a{x}"},
+                {"/ab", "/{x}b"},
         };
     }
 
@@ -44,4 +48,40 @@ public class UriPattenTest {
     public void testMatching(String a, String b) throws Exception {
         Assert.assertTrue(new UriPatten(a).match(b));
     }
+
+    @Test
+    public void testInvariants() throws Exception {
+        UriPatten[] pattens = new UriPatten[]{
+                new UriPatten("/"),
+                new UriPatten("/a"),
+                new UriPatten("/{a}"),
+                new UriPatten("/ab"),
+                new UriPatten("/a{b}"),
+                new UriPatten("/{a}/{b}"),
+                new UriPatten("/{a}"),
+                new UriPatten("/a/{b}"),
+                new UriPatten("/ab/{b}"),
+                new UriPatten("/{a}/b"),
+        };
+        //following invariants are specified in java.lang.Comparable
+        for (int x = 0; x < pattens.length; x++) {
+            for (int y = x + 1; y < pattens.length; y++) {
+                int xy = pattens[x].compareTo(pattens[y]);
+                int yx = pattens[y].compareTo(pattens[x]);
+                Assert.assertTrue(signum(xy) == -signum(yx));
+                for (int z = y + 1; z < pattens.length; z++) {
+                    int xz = pattens[x].compareTo(pattens[z]);
+                    int yz = pattens[y].compareTo(pattens[z]);
+                    if (xy > 0 && yz > 0) {
+                        Assert.assertTrue(xz > 0);
+                    } else if (yx > 0 && yz > 0) {
+                        Assert.assertTrue(yz > 0);
+                    } else if (xy == 0) {
+                        Assert.assertTrue(signum(xz) == signum(yz));
+                    }
+                }
+            }
+        }
+    }
+
 }
