845c982f54bbece8ce5dbae9ee08acfbb99fc4bd
Rasika Perera <info.rasika@gmail.com>
2016-03-15 16:06:55 +0530

    Adding static file routing

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/AppCreator.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/AppCreator.java
@@ -1,6 +1,14 @@
 package org.wso2.carbon.uuf.core;
 
+import java.nio.file.Path;
+
 public interface AppCreator {
 
+    String STATIC_RESOURCE_PREFIX="/public";
+    String STATIC_RESOURCE_PATH_PARAM_ROOT="root";
+    String STATIC_RESOURCE_PATH_PARAM_BASE="base";
+
     App createApp(String name, String context);
+
+    Path resolve(String appName, String resourcePath);
 }
--- uuf-core/src/main/java/org/wso2/carbon/uuf/fileio/FromArtifactAppCreator.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/fileio/FromArtifactAppCreator.java
@@ -1,5 +1,6 @@
 package org.wso2.carbon.uuf.fileio;
 
+import org.apache.commons.lang3.StringUtils;
 import org.wso2.carbon.uuf.core.*;
 
 import javax.ws.rs.core.Response;
@@ -11,7 +12,6 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -42,11 +42,7 @@ public class FromArtifactAppCreator implements AppCreator {
 
     private static Stream<? extends Path> findHbs(Path path, Path components) {
         try {
-            return Files
-                    .find(
-                            path,
-                            Integer.MAX_VALUE,
-                            (p, a) -> p.getFileName().toString().endsWith(".hbs"))
+            return Files.find(path, Integer.MAX_VALUE, (p, a) -> p.getFileName().toString().endsWith(".hbs"))
                     .map(components::relativize);
         } catch (IOException e) {
             throw new UUFException("error while finding a page", e);
@@ -59,39 +55,71 @@ public class FromArtifactAppCreator implements AppCreator {
         }
 
         LayoutCreator layoutCreator = new LayoutCreator(components);
-        List<Page> pages = Files
-                .list(components)
-                .flatMap(c -> subDirsOfAComponent(c, "pages"))
-                .flatMap(p -> findHbs(p, components))
-                .parallel()
-                .map(p -> pageCreator.createPage(p, layoutCreator, components))
-                .collect(Collectors.toList());
+        List<Page> pages = Files.list(components).flatMap(c -> subDirsOfAComponent(c, "pages")).flatMap(
+                p -> findHbs(p, components)).parallel().map(
+                p -> pageCreator.createPage(p, layoutCreator, components)).collect(Collectors.toList());
 
-        List<Fragment> fragments = Files
-                .list(components)
-                .flatMap(c -> subDirsOfAComponent(c, "fragments"))
-                .parallel()
-                .map(fragmentCreator::createFragment)
-                .collect(Collectors.toList());
+        List<Fragment> fragments = Files.list(components).flatMap(c -> subDirsOfAComponent(c, "fragments"))
+                .parallel().map(fragmentCreator::createFragment).collect(Collectors.toList());
         return new App(context, pages, fragments, new HashMap<>());
     }
 
-
     @Override
     public App createApp(String name, String context) {
+        try {
+            return createFromComponents(getAppPath(name).resolve("components"), context);
+        } catch (IOException e) {
+            throw new UUFException("error while creating app for '" + name + "'", e);
+        }
+    }
+
+    @Override
+    public Path resolve(String appName, String resourcePath) {
+        // component_resource_path: /public/<component_name>/base/js/lib.js
+        // fragment_resource_path: /public/<component_name>/<fragment_name>/js/lib.js
+        // root_resource_path: /public/root/base/js/lib.js
+        // root_fragment_path: /public/root/<fragment_name>/js/lib.js
+
+        String filePath = "";
+        Path appPath = getAppPath(appName);
+        String resourcePathParts[] = resourcePath.split("/");
+
+        int thirdSlash = StringUtils.ordinalIndexOf(resourcePath, "/", 3);
+        String subResourcePath = resourcePath.substring(thirdSlash, resourcePath.length());
+
+        if (resourcePathParts[0].equals(AppCreator.STATIC_RESOURCE_PREFIX)) {
+            if (resourcePathParts[1].equals(AppCreator.STATIC_RESOURCE_PATH_PARAM_ROOT)) {
+                if (resourcePathParts[2].equals(AppCreator.STATIC_RESOURCE_PATH_PARAM_BASE)) {
+                    //root_resource_path
+                    appPath.resolve(subResourcePath);
+                } else {
+                    //root_fragment_path
+                    appPath.resolve(subResourcePath);
+                }
+            } else {
+                if (resourcePathParts[2].equals(AppCreator.STATIC_RESOURCE_PATH_PARAM_BASE)) {
+                    //component_resource_path
+                    appPath.resolve(subResourcePath);
+                } else {
+                    //fragment_resource_path
+                    appPath.resolve(subResourcePath);
+                }
+            }
+        }
+
+        throw new UUFException("Resource by uri '" + resourcePath + "' is not found in " + appName,
+                Response.Status.NOT_FOUND);
+    }
+
+    private Path getAppPath(String name) {
         // app list mush be <white-space> and comma separated. <white-space> in app names not allowed
         for (String pathString : paths) {
             Path path = FileSystems.getDefault().getPath(pathString).toAbsolutePath().normalize();
             if (name.equals(path.getFileName().toString())) {
-                try {
-                    return createFromComponents(path.resolve("components"), context);
-                } catch (IOException e) {
-                    throw new UUFException("error while creating app for '" + name + "'", e);
-                }
+                return path;
             }
         }
-        throw new UUFException(
-                "app by the name '" + name + "' is not found in " + Arrays.toString(paths),
+        throw new UUFException("app by the name '" + name + "' is not found in " + Arrays.toString(paths),
                 Response.Status.NOT_FOUND);
     }
 }
