0f73d843c37939b9d8d403cd42dd6a49bd349a44
manuranga <manu@wso2.com>
2016-03-16 00:27:08 +0530

    adding call service

--- uuf-core/src/main/java/org/wso2/carbon/uuf/handlebars/JSExecutable.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/handlebars/JSExecutable.java
@@ -1,22 +1,38 @@
 package org.wso2.carbon.uuf.handlebars;
 
+import jdk.nashorn.api.scripting.AbstractJSObject;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import org.wso2.carbon.uuf.core.UUFException;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+@SuppressWarnings("PackageAccessibility")
 public class JSExecutable implements Executable {
 
-    private static final ScriptEngineManager engineManager = new ScriptEngineManager();
     private final Invocable engine;
     private final String fileName;
+    private static final NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+
+    private class MSSCaller extends AbstractJSObject {
+        @Override
+        public Object call(Object jsThis, Object... args) {
+            return "{}";
+        }
+
+        @Override
+        public boolean isFunction() {
+            return true;
+        }
+    }
 
     public JSExecutable(String script, String fileName) {
         this.fileName = fileName;
         try {
-            ScriptEngine engine = engineManager.getEngineByName("nashorn");
+            ScriptEngine engine = factory.getScriptEngine("-strict");
+            engine.put("MSSCaller", new MSSCaller());
+            engine.eval("var callService = function(method,uri){return JSON.parse(MSSCaller(method,uri))}");
             engine.eval(script + "//@ sourceURL=" + fileName);
             this.engine = (Invocable) engine;
         } catch (ScriptException e) {
