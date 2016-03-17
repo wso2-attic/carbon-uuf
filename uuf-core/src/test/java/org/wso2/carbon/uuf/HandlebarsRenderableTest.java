1b719b2151df4c7e44cf0d76edcda3e8c0ab50b7
sajithar <sajithar@wso2.com>
2016-03-17 12:44:39 +0530

    updated and added new test cases to test plain HBS template, fragments, zones, with JS script, etc.

--- uuf-core/src/test/java/org/wso2/carbon/uuf/HandlebarsRenderableTest.java
+++ uuf-core/src/test/java/org/wso2/carbon/uuf/HandlebarsRenderableTest.java
@@ -1,17 +1,15 @@
 package org.wso2.carbon.uuf;
 
-import com.github.jknack.handlebars.HandlebarsError;
-import com.github.jknack.handlebars.HandlebarsException;
-import com.github.jknack.handlebars.io.StringTemplateSource;
-import com.github.jknack.handlebars.io.TemplateSource;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Multimap;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.wso2.carbon.uuf.core.Fragment;
-import org.wso2.carbon.uuf.handlebars.HbsRenderable;
-import org.wso2.carbon.uuf.handlebars.HbsPageRenderable;
 import org.wso2.carbon.uuf.core.Renderable;
+import org.wso2.carbon.uuf.handlebars.Executable;
+import org.wso2.carbon.uuf.handlebars.HbsRenderable;
+import org.wso2.carbon.uuf.handlebars.JSExecutable;
 
 import java.util.Collections;
 import java.util.Map;
@@ -19,43 +17,55 @@ import java.util.Map;
 public class HandlebarsRenderableTest {
 
     @Test
-    public void testZones() {
-        TemplateSource source = new StringTemplateSource("my-file.hbs", "{{defineZone \"my-zone\"}}");
-        HbsRenderable hb = new HbsRenderable(source);
-        String output = hb.render(ImmutableMap.of("name", "Leela"),
-                                  ImmutableListMultimap.of("my-zone", new MockHelloRenderable()),
-                                  Collections.emptyMap());
-        Assert.assertEquals(output, "Welcome to the <world> of tomorrow, Leela");
+    public void testTemplate() {
+        final String templateContent = "A Plain Handlebars template.";
+        HbsRenderable hbsRenderable = new HbsRenderable(templateContent);
+        String output = hbsRenderable.render(new Object(), ImmutableListMultimap.of(), Collections.emptyMap());
+        Assert.assertEquals(output, templateContent);
+    }
+
+    @Test
+    public void testTemplateWithModel() {
+        HbsRenderable hbsRenderable = new HbsRenderable("Hello {{name}}! Have a good day.");
+        Map model = ImmutableMap.of("name", "Alice");
+        String output = hbsRenderable.render(model, ImmutableListMultimap.of(), Collections.emptyMap());
+        Assert.assertEquals(output, "Hello Alice! Have a good day.");
+    }
+
+    @Test
+    public void testTemplateWithExecutable() {
+        Executable executable = context -> ImmutableMap.of("name", "Alice");
+        HbsRenderable hbsRenderable = new HbsRenderable("Hello {{name}}! Have a good day.", executable);
+        String output = hbsRenderable.render(new Object(), ImmutableListMultimap.of(), Collections.emptyMap());
+        Assert.assertEquals(output, "Hello Alice! Have a good day.");
+    }
+
+    @Test
+    public void testTemplateWithJsExecutable() {
+        JSExecutable script = new JSExecutable("function onRequest(){ return {name: \"Alice\"}; }");
+        HbsRenderable hbsRenderable = new HbsRenderable("Hello {{name}}! Have a good day.", script);
+        String output = hbsRenderable.render(new Object(), ImmutableListMultimap.of(), Collections.emptyMap());
+        Assert.assertEquals(output, "Hello Alice! Have a good day.");
     }
 
     @Test
     public void testFragment() {
-        TemplateSource hbsTemplate = new StringTemplateSource("my-file.hbs", "{{includeFragment \"news\"}}");
-        HbsRenderable renderable = new HbsRenderable(hbsTemplate);
-        Fragment fragment = new Fragment("my-news-fragment", "/mock/path", (m, b, f) -> ("Good news, " + m + "!"));
-        String output = renderable.render("everyone", ImmutableListMultimap.of(), ImmutableMap.of("news", fragment));
-        Assert.assertEquals(output, "Good news, everyone!");
+        HbsRenderable hbsRenderable = new HbsRenderable("{{includeFragment \"test-fragment\"}}");
+        final String fragmentContent = "This is the content of the test-fragment.";
+        HbsRenderable fragmentRenderable = new HbsRenderable(fragmentContent);
+        Fragment fragment = new Fragment("test-fragment", "/mock/path", fragmentRenderable);
+        String output = hbsRenderable.render(new Object(), ImmutableListMultimap.of(), ImmutableMap.of("test-fragment",
+                                                                                                       fragment));
+        Assert.assertEquals(output, fragmentContent);
     }
 
     @Test
-    public void testFillingZones() {
-        TemplateSource hbsTemplate = new StringTemplateSource("my-file.hbs",
-                                                              "\n{{#fillZone \"my-zone\"}} {{a}}{{/fillZone}}");
-        HbsPageRenderable hbsPageRenderable = new HbsPageRenderable(hbsTemplate);
-        Map<String, Renderable> fillingZones = hbsPageRenderable.getFillingZones();
-        Renderable fillingZone = fillingZones.get("my-zone");
-        Assert.assertNotNull(fillingZone, "zone's inner content must be available under name 'my-zone'");
-        try {
-            fillingZone.render(Collections.emptyMap(), ImmutableListMultimap.of(), Collections.emptyMap());
-            Assert.fail("can't render with an empty map since 'a' var is expected.");
-        } catch (HandlebarsException ex) {
-            HandlebarsError error = ex.getError();
-            Assert.assertEquals(error.line, 2, "error is in the 2nd line");
-            Assert.assertEquals(error.column, 26, "error is in the 26th column");
-        }
-
-        String rendered = fillingZone.render(ImmutableMap.of("a", "apple"), ImmutableListMultimap.of(),
-                                             Collections.emptyMap());
-        Assert.assertEquals(rendered.trim(), "apple");
+    public void testZones() {
+        HbsRenderable defineZoneRenderable = new HbsRenderable("{{defineZone \"test-zone\"}}");
+        final String zoneContent = "This is the content of the test-zone.";
+        HbsRenderable fillZoneRenderable = new HbsRenderable(zoneContent);
+        Multimap<String, Renderable> bindings = ImmutableListMultimap.of("test-zone", fillZoneRenderable);
+        String output = defineZoneRenderable.render(new Object(), bindings, Collections.emptyMap());
+        Assert.assertEquals(output, zoneContent);
     }
 }
