375de842771005a417904c100211c40a46fb0f6d
sajithar <sajithar@wso2.com>
2016-03-26 09:53:22 +0530

    now ResourceHelper gets the resource's public URI prefix from the current fragment

--- uuf-core/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/runtime/ResourceHelper.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/runtime/ResourceHelper.java
@@ -3,11 +3,15 @@ package org.wso2.carbon.uuf.handlebars.helpers.runtime;
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
+import org.wso2.carbon.uuf.core.Fragment;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.Optional;
+
+import static org.wso2.carbon.uuf.handlebars.HbsRenderable.FRAGMENTS_STACK_KEY;
 
 public abstract class ResourceHelper implements Helper<String> {
 
@@ -18,16 +22,25 @@ public abstract class ResourceHelper implements Helper<String> {
     }
 
     @Override
-    public CharSequence apply(String uri, Options options) throws IOException {
-        if (!uri.startsWith("/")) {
-            throw new IllegalArgumentException("A relative public URI should start with '/'.");
+    public CharSequence apply(String relativeUri, Options options) throws IOException {
+        if (!relativeUri.startsWith("/")) {
+            throw new IllegalArgumentException("A relative URI should start with '/'.");
         }
         List<String> resources = options.data(resourcesCategory);
         if (resources == null) {
             resources = new ArrayList<>();
             options.data(resourcesCategory, resources);
         }
-        resources.add(format(uri));
+        Deque<Fragment> fragmentStack = options.data(FRAGMENTS_STACK_KEY);
+        String publicUri;
+        if ((fragmentStack == null) || fragmentStack.isEmpty()) {
+            // this resource is adding in a page or layout
+            //TODO get component's public URI
+            publicUri = "/public/component-name/base" + relativeUri;
+        } else {
+            publicUri = fragmentStack.peekLast().getResourceUriPrefix() + relativeUri;
+        }
+        resources.add(format(publicUri));
         return "";
     }
 
