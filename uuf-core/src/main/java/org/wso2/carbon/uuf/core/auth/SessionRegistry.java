22629d5030cac61c9a8350080b7036e543194c9b
sajithar <sajithar@wso2.com>
2016-04-04 19:28:01 +0530

    added SessionRegistry class

---
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/auth/SessionRegistry.java
@@ -0,0 +1,53 @@
+package org.wso2.carbon.uuf.core.auth;
+
+import javax.cache.Cache;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.AccessedExpiryPolicy;
+import javax.cache.expiry.Duration;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Optional;
+
+public class SessionRegistry implements Closeable {
+    private Cache<String, Session> cache;
+
+    public SessionRegistry() {
+        MutableConfiguration<String, Session> config = new MutableConfiguration<>();
+        config.setTypes(String.class, Session.class);
+        config.setStoreByValue(true);
+        // TODO read session expire time from a config
+        config.setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(Duration.TWENTY_MINUTES));
+        String cacheName = this.getClass().getName() + "-sessions_cache";
+        cache = Caching.getCachingProvider().getCacheManager().createCache(cacheName, config);
+    }
+
+    public void addSession(Session session) {
+        cache.put(session.getSessionId(), session);
+    }
+
+    public Optional<Session> getSession(String token) {
+        return Optional.ofNullable(cache.get(token));
+    }
+
+    public boolean removeSession(String token) {
+        return cache.remove(token);
+    }
+
+    public void removeAllSessions() {
+        cache.clear();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (!cache.isClosed()) {
+            cache.close();
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        close();
+        super.finalize();
+    }
+}
