7c5b8870578f1b2d7c40962c9fa263007d9085a2
sajithar <sajithar@wso2.com>
2016-04-06 23:39:10 +0530

    in App class, added a constructor to accept a SessionRegistry as an argument & added a 'renderPage' method that accept a String uri and a DynamicLookup as arguments

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/App.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/App.java
@@ -1,5 +1,7 @@
 package org.wso2.carbon.uuf.core;
 
+import org.wso2.carbon.uuf.core.auth.SessionRegistry;
+
 import javax.ws.rs.core.Response;
 import java.util.Map;
 import java.util.Optional;
@@ -8,10 +10,24 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class App {
+    private static final String ROOT_COMPONENT_CONTEXT = "/root";
     private final String context;
     private final Map<String, Component> components;
     private final Component rootComponent;
+    private final SessionRegistry sessionRegistry;
 
+    public App(String context, Set<Component> components, SessionRegistry sessionRegistry) {
+        if (!context.startsWith("/")) {
+            throw new IllegalArgumentException("App context must start with a '/'.");
+        }
+
+        this.context = context;
+        this.components = components.stream().collect(Collectors.toMap(Component::getContext, Function.identity()));
+        this.rootComponent = this.components.get(ROOT_COMPONENT_CONTEXT);
+        this.sessionRegistry = sessionRegistry;
+    }
+
+    @Deprecated
     public App(String context, Set<Component> components) {
         if (!context.startsWith("/")) {
             throw new IllegalArgumentException("app context must start with a '/'");
@@ -20,8 +36,34 @@ public class App {
         this.context = context;
         this.components = components.stream().collect(Collectors.toMap(Component::getContext, Function.identity()));
         this.rootComponent = this.components.get("/root");
+
+        this.sessionRegistry = null;
     }
 
+    public String renderPage(String uriWithoutContext, DynamicLookup dynamicLookup) {
+        // First try to render the page with root component
+        Optional<String> output = rootComponent.renderPage(uriWithoutContext, dynamicLookup);
+        if (output.isPresent()) {
+            return output.get();
+        }
+
+        // Since root components doesn't have the page, try with other components
+        for (Map.Entry<String, Component> entry : components.entrySet()) {
+            if (uriWithoutContext.startsWith(entry.getKey())) {
+                Component component = entry.getValue();
+                String pageUri = uriWithoutContext.substring(component.getContext().length());
+                output = component.renderPage(pageUri, dynamicLookup);
+                if (output.isPresent()) {
+                    return output.get();
+                }
+                break;
+            }
+        }
+        throw new UUFException("Requested page '" + uriWithoutContext + "' does not exists.",
+                               Response.Status.NOT_FOUND);
+    }
+
+    @Deprecated
     public String renderPage(String uriUpToContext, String uriAfterContext) {
         // First try to render the page with root component
         Optional<String> output = rootComponent.renderPage(uriUpToContext, uriAfterContext);
@@ -67,5 +109,13 @@ public class App {
 
     public Map<String, Component> getComponents() {
         return components;
+    }
+
+    public SessionRegistry getSessionRegistry() {
+        return sessionRegistry;
+    }
+
+    public String getContext() {
+        return context;
     }
 }
