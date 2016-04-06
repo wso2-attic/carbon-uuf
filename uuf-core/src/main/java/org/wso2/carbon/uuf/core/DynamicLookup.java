cc3b88ba4513dd1dc4542e6e5a6eea373088889b
sajithar <sajithar@wso2.com>
2016-04-06 23:34:47 +0530

    added DynamicLookup class

---
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/DynamicLookup.java
@@ -0,0 +1,37 @@
+package org.wso2.carbon.uuf.core;
+
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DynamicLookup {
+    private final HttpRequest request;
+    private final HttpResponse response;
+    private final Deque<Page> pagesStack;
+    private final Deque<Fragment> fragmentsStack;
+
+    public DynamicLookup(HttpRequest request, HttpResponse response) {
+        this.request = request;
+        this.response = response;
+        this.pagesStack = new ArrayDeque<>();
+        this.fragmentsStack = new ArrayDeque<>();
+    }
+
+    public HttpRequest getRequest() {
+        return request;
+    }
+
+    public HttpResponse getResponse() {
+        return response;
+    }
+
+    public Deque<Page> getPagesStack() {
+        return pagesStack;
+    }
+
+    public Deque<Fragment> getFragmentsStack() {
+        return fragmentsStack;
+    }
+}
