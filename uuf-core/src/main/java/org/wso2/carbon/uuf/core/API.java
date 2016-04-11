0d970fdeba27006c5f26168a732e3afe159beae5
sajithar <sajithar@wso2.com>
2016-04-11 17:05:00 +0530

    added empty methods to the API class

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/API.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/API.java
@@ -2,5 +2,27 @@ package org.wso2.carbon.uuf.core;
 
 public class API {
 
+    public void callOSGiService() {
+        throw new UnsupportedOperationException("To be implemented");
+    }
 
+    public void callMicroService() {
+        throw new UnsupportedOperationException("To be implemented");
+    }
+
+    public void createSession() {
+        throw new UnsupportedOperationException("To be implemented");
+    }
+
+    public void sendError(int status, String message) {
+        throw new UnsupportedOperationException("To be implemented");
+    }
+
+    public void sendRedirect(String url) {
+        throw new UnsupportedOperationException("To be implemented");
+    }
+
+    public void setTheme(String name) {
+        throw new UnsupportedOperationException("To be implemented");
+    }
 }
