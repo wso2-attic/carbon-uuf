24c27645bc84ba8cd7b08329b6a195cc0cfc5892
sajithar <sajithar@wso2.com>
2016-04-18 15:22:17 +0530

    adde a constructor and changed 'render' method to be comply with new updates

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/Fragment.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/Fragment.java
@@ -5,12 +5,16 @@ import org.wso2.carbon.uuf.model.Model;
 public class Fragment {
 
     private final String name;
-    private final String publicUriInfix;
     private final Renderable renderer;
 
+    public Fragment(String name, Renderable renderer) {
+        this.name = name;
+        this.renderer = renderer;
+    }
+
+    @Deprecated
     public Fragment(String name, String publicUriInfix, Renderable renderer) {
         this.name = name;
-        this.publicUriInfix = publicUriInfix;
         this.renderer = renderer;
     }
 
@@ -18,18 +22,10 @@ public class Fragment {
         return name;
     }
 
-    public String getPublicUriInfix() {
-        return publicUriInfix;
-    }
-
-    public Renderable getRenderer() {
-        return renderer;
-    }
-
-    public String render(Model model, StaticLookup staticLookup, RequestLookup requestLookup, API api) {
-        requestLookup.getFragmentsStack().push(this);
-        String output = renderer.render(model, staticLookup, requestLookup, api);
-        requestLookup.getFragmentsStack().pop();
+    public String render(Model model, ComponentLookup componentLookup, RequestLookup requestLookup, API api) {
+        requestLookup.pushToPublicUriStack(requestLookup.getAppContext() + componentLookup.getPublicUriInfix(this));
+        String output = renderer.render(model, componentLookup, requestLookup, api);
+        requestLookup.popPublicUriStack();
         return output;
     }
 
@@ -40,11 +36,15 @@ public class Fragment {
 
     @Override
     public String toString() {
-        return "{\"name\": \"" + name + "\", \"publicUriInfix\": \"" + publicUriInfix + "\"}";
+        return "{\"name\": \"" + name + "\", \"renderer\": \"" + renderer + "\"}";
     }
 
     @Deprecated
     public String getPublicContext() {
-        return publicUriInfix;
+        return "";
+    }
+
+    public Renderable getRenderer() {
+        return renderer;
     }
 }
