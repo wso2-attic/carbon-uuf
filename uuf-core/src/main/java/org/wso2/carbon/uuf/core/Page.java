b7a646045ccb648fb0000a82ab7341c2b56b42c9
sajithar <sajithar@wso2.com>
2016-04-18 15:23:14 +0530

    changed 'render' method to be comply with new updates

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/Page.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/Page.java
@@ -6,18 +6,15 @@ public class Page implements Comparable<Page> {
 
     private final UriPatten uriPatten;
     private final Renderable renderer;
-    private final String publicUriInfix;
 
     @Deprecated
     public Page(UriPatten uriPatten, Renderable layout, Lookup lookup) {
         this.uriPatten = uriPatten;
         this.renderer = layout;
-        this.publicUriInfix = null;
     }
 
-    public Page(UriPatten uriPatten, String publicUriInfix, Renderable renderer) {
+    public Page(UriPatten uriPatten, Renderable renderer) {
         this.uriPatten = uriPatten;
-        this.publicUriInfix = publicUriInfix;
         this.renderer = renderer;
     }
 
@@ -25,14 +22,10 @@ public class Page implements Comparable<Page> {
         return uriPatten;
     }
 
-    public String getPublicUriInfix() {
-        return publicUriInfix;
-    }
-
-    public String render(Model model, StaticLookup staticLookup, RequestLookup requestLookup, API api) {
-        requestLookup.getPagesStack().push(this);
-        String output = renderer.render(model, staticLookup, requestLookup, api);
-        requestLookup.getPagesStack().pop();
+    public String render(Model model, ComponentLookup componentLookup, RequestLookup requestLookup, API api) {
+        requestLookup.pushToPublicUriStack(requestLookup.getAppContext() + componentLookup.getPublicUriInfix(this));
+        String output = renderer.render(model, componentLookup, requestLookup, api);
+        requestLookup.popPublicUriStack();
         return output;
     }
 
@@ -43,7 +36,7 @@ public class Page implements Comparable<Page> {
 
     @Override
     public String toString() {
-        return "{\"uriPattern\": \"" + uriPatten.toString() + "\", \"publicUriPrefix\": \"" + publicUriInfix + "\"}";
+        return "{\"uriPattern\": \"" + uriPatten.toString() + "\", \"renderer\": \"" + renderer + "\"}";
     }
 
     @Override
