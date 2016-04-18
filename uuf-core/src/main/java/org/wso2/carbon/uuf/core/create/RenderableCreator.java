14afbaa94167086b3bfc6859b0bf3f2f51b9d100
sajithar <sajithar@wso2.com>
2016-04-18 15:11:47 +0530

    renamed two methods in RenderableCreator interface

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/create/RenderableCreator.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/create/RenderableCreator.java
@@ -4,14 +4,13 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.wso2.carbon.uuf.core.Renderable;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 public interface RenderableCreator {
-    Optional<Renderable> createRenderable(FileReference fileReference, ClassLoader cl);
-
-    Optional<Pair<Renderable, Map<String, ? extends Renderable>>>
-    createRenderableWithBindings(FileReference pageReference, ClassLoader loader);
-
     Set<String> getSupportedFileExtensions();
+
+    Renderable createFragmentRenderable(FragmentReference fragmentReference, ClassLoader classLoader);
+
+    Pair<Renderable, Map<String, ? extends Renderable>> createPageRenderables(PageReference pageReference,
+                                                                              ClassLoader classLoader);
 }
