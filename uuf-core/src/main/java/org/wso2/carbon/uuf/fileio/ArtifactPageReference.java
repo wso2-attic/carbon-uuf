96de576218a707bc721562e7ab519af63d584bff
sajithar <sajithar@wso2.com>
2016-04-19 13:13:00 +0530

    refactored ArtifactPageReference class to be comply with new changes of PageReference interface

--- uuf-core/src/main/java/org/wso2/carbon/uuf/fileio/ArtifactPageReference.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/fileio/ArtifactPageReference.java
@@ -1,25 +1,49 @@
 package org.wso2.carbon.uuf.fileio;
 
+import org.wso2.carbon.uuf.core.create.AppReference;
+import org.wso2.carbon.uuf.core.create.ComponentReference;
 import org.wso2.carbon.uuf.core.create.FileReference;
 import org.wso2.carbon.uuf.core.create.PageReference;
 
+import java.nio.file.Path;
+
 public class ArtifactPageReference implements PageReference {
 
-    private final String pathPattern;
-    private final FileReference renderingFile;
+    private final Path path;
+    private final ArtifactAppReference appReference;
+    private final ArtifactComponentReference componentReference;
 
-    public ArtifactPageReference(String pathPattern, FileReference renderingFile) {
-        this.pathPattern = pathPattern;
-        this.renderingFile = renderingFile;
+    public ArtifactPageReference(Path path, ArtifactComponentReference componentReference,
+                                 ArtifactAppReference appReference) {
+        this.path = path;
+        this.componentReference = componentReference;
+        this.appReference = appReference;
     }
 
     @Override
     public String getPathPattern() {
-        return pathPattern;
+        StringBuilder sb = new StringBuilder();
+        for (Path path : componentReference.getPath().resolve("pages").relativize(this.path)) {
+            sb.append('/');
+            sb.append(path.toString());
+        }
+        return sb.toString();
     }
 
     @Override
     public FileReference getRenderingFile() {
-        return renderingFile;
+        return new ArtifactFileReference(path, componentReference);
+    }
+
+    @Deprecated
+    @Override
+    public AppReference getAppReference() {
+        return appReference;
+    }
+
+    @Deprecated
+    @Override
+    public ComponentReference getComponentReference() {
+        return componentReference;
     }
 }
