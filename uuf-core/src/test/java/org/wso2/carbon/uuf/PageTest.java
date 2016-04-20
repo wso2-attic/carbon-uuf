bffefef902de719df058265cdf58d7f7b146ff55
sajithar <sajithar@wso2.com>
2016-04-20 15:24:20 +0530

    fixed PageTest test case

--- uuf-core/src/test/java/org/wso2/carbon/uuf/PageTest.java
+++ uuf-core/src/test/java/org/wso2/carbon/uuf/PageTest.java
@@ -1,9 +1,14 @@
 package org.wso2.carbon.uuf;
 
+import com.google.common.collect.ImmutableSetMultimap;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.wso2.carbon.uuf.core.ComponentLookup;
 import org.wso2.carbon.uuf.core.Page;
 import org.wso2.carbon.uuf.core.Renderable;
+import org.wso2.carbon.uuf.core.RequestLookup;
+
+import java.util.Collections;
 
 import static org.mockito.Matchers.any;
 
@@ -11,10 +16,14 @@ public class PageTest {
 
     @Test
     public void testRenderPage() throws Exception {
-        Renderable renderable = (model, componentLookup, requestLookup, api) -> "Hello world!";
+        final String content = "Hello world from a page!";
+        Renderable renderable = (model, componentLookup, requestLookup, api) -> content;
+        ComponentLookup lookup = new ComponentLookup("componentName", "/componentContext", Collections.emptySet(),
+                                                     ImmutableSetMultimap.of(), Collections.emptySet());
+        RequestLookup requestLookup = new RequestLookup("/appContext", any());
         Page page = new Page(any(), renderable);
-        String output = page.render(any(), any(), any(), any());
-        Assert.assertEquals(output, "Hello world!");
-    }
 
+        String output = page.render(any(), lookup, requestLookup, any());
+        Assert.assertEquals(output, content);
+    }
 }
