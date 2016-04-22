0be478f0ab14201a8b6708cfeb45563439c27e9b
sajithar <sajithar@wso2.com>
2016-04-22 10:40:45 +0530

    in FillZoneHelper class: used 'DATA_KEY_*' constants from HbsInitRenderable class, code formatting, descriptive error messages

--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/init/FillZoneHelper.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/init/FillZoneHelper.java
@@ -4,24 +4,26 @@ import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
 import com.github.jknack.handlebars.io.StringTemplateSource;
 import com.github.jknack.handlebars.io.TemplateSource;
-import org.wso2.carbon.uuf.handlebars.HbsInitRenderable;
 import org.wso2.carbon.uuf.core.Renderable;
 import org.wso2.carbon.uuf.core.UUFException;
+import org.wso2.carbon.uuf.handlebars.HbsInitRenderable;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.wso2.carbon.uuf.handlebars.HbsInitRenderable.DATA_KEY_ZONES;
+
 public class FillZoneHelper implements Helper<String> {
+
     public static final String HELPER_NAME = "fillZone";
-    public static final String ZONES_KEY = FillZoneHelper.class.getName() + "#zones";
 
     @Override
     public CharSequence apply(String zoneName, Options options) throws IOException {
-        Map<String, Renderable> zones = options.data(ZONES_KEY);
+        Map<String, Renderable> zones = options.data(DATA_KEY_ZONES);
 
-        //we will prepend the source with spaces and enters to make the line numbers correct
+        // We will prepend the source with spaces and enters to make the line numbers correct.
         int line = options.fn.position()[0];
         String source;
         if (line > 0) {
@@ -43,14 +45,13 @@ public class FillZoneHelper implements Helper<String> {
         }
         if (zones == null) {
             zones = new HashMap<>();
-            options.data(ZONES_KEY, zones);
+            options.data(DATA_KEY_ZONES, zones);
         }
-        TemplateSource templateSource = new StringTemplateSource(
-                options.fn.filename(),
-                source);
+        TemplateSource templateSource = new StringTemplateSource(options.fn.filename(), source);
         HbsInitRenderable fillZoneRenderable = new HbsInitRenderable(templateSource, Optional.empty());
         if (fillZoneRenderable.getFillingZones().size() > 0) {
-            throw new UUFException("fill zones inside fill zones are not valid");
+            throw new UUFException("Calling '" + HELPER_NAME + "' helper inside of another '" + HELPER_NAME +
+                                           "' helper block is prohibited.");
         }
         zones.put(zoneName, fillZoneRenderable);
         return "";
