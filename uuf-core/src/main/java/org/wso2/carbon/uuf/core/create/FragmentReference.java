a0f0578ae5f598f44146a7f9aa7133903d66723a
sajithar <sajithar@wso2.com>
2016-04-22 11:34:21 +0530

    removed unused constant from FragmentReference interface

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/create/FragmentReference.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/create/FragmentReference.java
@@ -2,8 +2,6 @@ package org.wso2.carbon.uuf.core.create;
 
 public interface FragmentReference {
 
-    String PUBLIC_DIR_NAME = "public";
-
     String getName();
 
     FileReference getRenderingFile();
