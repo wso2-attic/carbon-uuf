0b4465929fcb1a0237bc429c61c6e1c69d91dfae
sajithar <sajithar@wso2.com>
2016-04-22 10:41:08 +0530

    in LayoutHelper class: used 'DATA_KEY_*' constants from HbsInitRenderable class, code formatting, descriptive error messages

--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/init/LayoutHelper.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/init/LayoutHelper.java
@@ -2,24 +2,25 @@ package org.wso2.carbon.uuf.handlebars.helpers.init;
 
 import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
-import org.wso2.carbon.uuf.handlebars.HbsInitRenderable;
 import org.wso2.carbon.uuf.core.UUFException;
 
 import java.io.IOException;
 
+import static org.wso2.carbon.uuf.handlebars.HbsInitRenderable.DATA_KEY_CURRENT_LAYOUT;
+
 public class LayoutHelper implements Helper<String> {
 
     public static final String HELPER_NAME = "layout";
-    public static final String LAYOUT_KEY = HbsInitRenderable.class.getName() + "#layout";
 
     @Override
     public CharSequence apply(String layoutName, Options options) throws IOException {
-        Object originalLayout = options.data(LAYOUT_KEY);
-        if (originalLayout != null) {
-            throw new UUFException(
-                    "multiple layout '" + layoutName + "','" + originalLayout + "'  defined");
+        Object currentLayout = options.data(DATA_KEY_CURRENT_LAYOUT);
+        if (currentLayout != null) {
+            throw new UUFException("Cannot set layout '" + layoutName + "' to this page because layout '" +
+                                           currentLayout + "' is already set.");
         }
-        options.data(LAYOUT_KEY, layoutName);
+        // TODO: 4/22/16 Validate 'layoutName'
+        options.data(DATA_KEY_CURRENT_LAYOUT, layoutName);
         return "";
     }
 }
