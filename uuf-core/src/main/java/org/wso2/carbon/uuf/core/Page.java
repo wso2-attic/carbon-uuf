dd240faabf899033c37c72f87ba4340bb84225dd
sajithar <sajithar@wso2.com>
2016-04-25 22:50:42 +0530

    added Layout to Page

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/Page.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/Page.java
@@ -2,14 +2,22 @@ package org.wso2.carbon.uuf.core;
 
 import org.wso2.carbon.uuf.model.Model;
 
+import java.util.Optional;
+
 public class Page implements Comparable<Page> {
 
     private final UriPatten uriPatten;
     private final Renderable renderer;
+    private final Optional<Layout> layout;
 
     public Page(UriPatten uriPatten, Renderable renderer) {
+        this(uriPatten, renderer, null);
+    }
+
+    public Page(UriPatten uriPatten, Renderable renderer, Layout layout) {
         this.uriPatten = uriPatten;
         this.renderer = renderer;
+        this.layout = Optional.ofNullable(layout);
     }
 
     public UriPatten getUriPatten() {
@@ -20,6 +28,9 @@ public class Page implements Comparable<Page> {
         requestLookup.pushToPublicUriStack(requestLookup.getAppContext() + componentLookup.getPublicUriInfix(this));
         String output = renderer.render(model, componentLookup, requestLookup, api);
         requestLookup.popPublicUriStack();
+        if (layout.isPresent()) {
+            output = layout.get().render(componentLookup, requestLookup);
+        }
         return output;
     }
 
