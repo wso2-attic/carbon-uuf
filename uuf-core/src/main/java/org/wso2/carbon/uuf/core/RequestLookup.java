bc67ff8a2c21fce30c115a676d101ca9ada5ef9c
sajithar <sajithar@wso2.com>
2016-04-27 00:48:31 +0530

    added 'addToPLaceholder', 'getPlaceholderContent', 'putToZone', 'getZoneContent' methods to RequestLookup class

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/RequestLookup.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/RequestLookup.java
@@ -4,16 +4,23 @@ import io.netty.handler.codec.http.HttpRequest;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class RequestLookup {
     private final String appContext;
     private final HttpRequest request;
     private final Deque<String> publicUriStack;
+    private final Map<String, StringBuilder> placeholderBuffers;
+    private final Map<String, String> zoneContents;
 
     public RequestLookup(String appContext, HttpRequest request) {
         this.appContext = appContext;
         this.request = request;
         this.publicUriStack = new ArrayDeque<>();
+        this.placeholderBuffers = new HashMap<>();
+        this.zoneContents = new HashMap<>();
     }
 
     public HttpRequest getRequest() {
@@ -34,5 +41,33 @@ public class RequestLookup {
 
     public String getPublicUri() {
         return publicUriStack.peekLast();
+    }
+
+    public void addToPLaceholder(String placeholderName, String content) {
+        StringBuilder buffer = placeholderBuffers.get(placeholderName);
+        if (buffer == null) {
+            buffer = new StringBuilder(content);
+            placeholderBuffers.put(placeholderName, buffer);
+        } else {
+            buffer.append(content);
+        }
+    }
+
+    public Optional<String> getPlaceholderContent(String placeholderName) {
+        StringBuilder buffer = placeholderBuffers.get(placeholderName);
+        return (buffer == null) ? Optional.<String>empty() : Optional.of(buffer.toString());
+    }
+
+    public void putToZone(String zoneName, String content) {
+        String currentContent = zoneContents.get(zoneName);
+        if (currentContent == null) {
+            zoneContents.put(zoneName, content);
+        } else {
+            throw new IllegalStateException("Zone '" + zoneName + "' is already filled with content.");
+        }
+    }
+
+    public Optional<String> getZoneContent(String zoneName) {
+        return Optional.ofNullable(zoneContents.get(zoneName));
     }
 }
