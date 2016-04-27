77b33e9ade18a22d23101272303c82565c3c9ddc
Rasika Perera <info.rasika@gmail.com>
2016-04-27 23:49:15 +0530

    UUF Exception modified

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/exception/UUFException.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/exception/UUFException.java
@@ -1,27 +1,19 @@
 package org.wso2.carbon.uuf.core.exception;
 
-import javax.ws.rs.core.Response;
-
-import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
-
 public class UUFException extends RuntimeException {
-    private final Response.Status status;
+
+    public UUFException() {
+    }
+
+    public UUFException(Throwable cause) {
+        super(cause);
+    }
 
     public UUFException(String message, Throwable cause) {
         super(message, cause);
-        this.status = INTERNAL_SERVER_ERROR;
     }
 
     public UUFException(String message) {
-        this(message, INTERNAL_SERVER_ERROR);
-    }
-
-    public UUFException(String message, Response.Status status) {
         super(message);
-        this.status = status;
-    }
-
-    public Response.Status getStatus() {
-        return status;
     }
 }
