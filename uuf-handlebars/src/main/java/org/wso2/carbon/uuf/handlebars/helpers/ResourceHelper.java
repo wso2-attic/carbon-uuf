16d831998a9d896b2e31753eed66ff14b035ddeb
sajithar <sajithar@wso2.com>
2016-04-27 21:11:02 +0530

    removed unnecessary 'getPlaceholderValue' method from ResourceHelper class

--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/ResourceHelper.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/ResourceHelper.java
@@ -1,21 +1,13 @@
 package org.wso2.carbon.uuf.handlebars.helpers;
 
-import com.github.jknack.handlebars.Context;
-import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Options;
 import org.wso2.carbon.uuf.core.RequestLookup;
-import org.wso2.carbon.uuf.handlebars.helpers.FillPlaceholderHelper;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 import static org.wso2.carbon.uuf.handlebars.HbsRenderable.DATA_KEY_REQUEST_LOOKUP;
 
-public abstract class ResourceHelper extends FillPlaceholderHelper<List<String>> {
-
-    private static final Handlebars.SafeString EMPTY_STRING = new Handlebars.SafeString("");
+public abstract class ResourceHelper extends FillPlaceholderHelper {
 
     public ResourceHelper(String resourcesCategory) {
         super(resourcesCategory);
@@ -28,20 +20,8 @@ public abstract class ResourceHelper extends FillPlaceholderHelper<List<String>>
         }
 
         RequestLookup requestLookup = options.data(DATA_KEY_REQUEST_LOOKUP);
-        List<String> resources = getValue(options).orElseGet(() -> setValue(options, new ArrayList<>()));
-        resources.add(formatResourceUri(requestLookup.getPublicUri() + relativeUri));
-        return EMPTY_STRING;
-    }
-
-    @Override
-    public Optional<String> getPlaceholderValue(Context context) {
-        return getValue(context).map(resources -> {
-            StringBuilder tmpBuffer = new StringBuilder();
-            for (String item : resources) {
-                tmpBuffer.append(item);
-            }
-            return tmpBuffer.toString();
-        });
+        requestLookup.putToZone(placeholderName, formatResourceUri(requestLookup.getPublicUri() + relativeUri));
+        return "";
     }
 
     protected abstract String formatResourceUri(String resourceUri);
