c617c53290bc36ad17058e8e126c98a069da497d
sajithar <sajithar@wso2.com>
2016-04-27 12:50:41 +0530

    changed HbsInitRenderable to extract only the layout name of a page

--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/HbsInitRenderable.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/HbsInitRenderable.java
@@ -2,49 +2,36 @@ package org.wso2.carbon.uuf.handlebars;
 
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.TemplateSource;
 import org.wso2.carbon.uuf.core.exception.UUFException;
-import org.wso2.carbon.uuf.handlebars.helpers.init.FillZoneHelper;
 import org.wso2.carbon.uuf.handlebars.helpers.init.LayoutHelper;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Optional;
 
-public class HbsInitRenderable extends HbsRenderable {
+public class HbsInitRenderable {
 
-    public static final String DATA_KEY_ZONES = HbsInitRenderable.class.getName() + "#zones";
     public static final String DATA_KEY_CURRENT_LAYOUT = HbsInitRenderable.class.getName() + "#layout";
     private static final Handlebars HANDLEBARS = new Handlebars();
 
     static {
-        HANDLEBARS.registerHelper(FillZoneHelper.HELPER_NAME, new FillZoneHelper());
         HANDLEBARS.registerHelper(LayoutHelper.HELPER_NAME, new LayoutHelper());
         HANDLEBARS.registerHelperMissing((context, options) -> "");
     }
 
-    private final Map<String, HbsInitRenderable> fillingZone;
     private final Optional<String> layout;
 
-    public HbsInitRenderable(TemplateSource template, Optional<Executable> executable) {
-        super(template, executable);
-        Template compiledTemplate;
+    public HbsInitRenderable(TemplateSource template) {
+        String templatePath = template.filename();
         Context context = Context.newContext(Collections.emptyMap());
         try {
-            compiledTemplate = HANDLEBARS.compile(template);
-            compiledTemplate.apply(context);
+            HANDLEBARS.compile(template).apply(context);
         } catch (IOException e) {
-            throw new UUFException("pages template completions error", e);
+            throw new UUFException(
+                    "An error occurred when pre-processing the Handlebars template of page '" + templatePath + "'.", e);
         }
-        Map<String, HbsInitRenderable> zones = context.data(DATA_KEY_ZONES);
-        fillingZone = (zones == null) ? Collections.emptyMap() : zones;
         layout = Optional.ofNullable(context.data(DATA_KEY_CURRENT_LAYOUT));
-    }
-
-    public Map<String, HbsInitRenderable> getFillingZones() {
-        return fillingZone;
     }
 
     public Optional<String> getLayoutName() {
