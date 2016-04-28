9a40d23b57edad1b8c69158e01b6b29c07281bb0
sajithar <sajithar@wso2.com>
2016-04-29 02:28:07 +0530

    fixed 'TODO: set context for executable' in HbsPageRenderable class 'render' method

--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/HbsPageRenderable.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/HbsPageRenderable.java
@@ -2,26 +2,26 @@ package org.wso2.carbon.uuf.handlebars;
 
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.io.TemplateSource;
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.carbon.uuf.core.API;
 import org.wso2.carbon.uuf.core.ComponentLookup;
 import org.wso2.carbon.uuf.core.RequestLookup;
 import org.wso2.carbon.uuf.core.exception.UUFException;
-import org.wso2.carbon.uuf.handlebars.model.ContextModel;
 import org.wso2.carbon.uuf.model.Model;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-
 public class HbsPageRenderable extends HbsRenderable {
 
-    private static final Logger log = LoggerFactory.getLogger(HbsRenderable.class);
+    private static final Logger log = LoggerFactory.getLogger(HbsPageRenderable.class);
 
-    private final Optional<Executable> executable;
+    protected final Optional<Executable> executable;
 
     public HbsPageRenderable(TemplateSource template) {
         this(template, Optional.<Executable>empty());
@@ -36,28 +36,18 @@ public class HbsPageRenderable extends HbsRenderable {
         this.executable = executable;
     }
 
-    public Optional<Executable> getScript() {
-        return executable;
-    }
-
     @Override
     public String render(Model model, ComponentLookup componentLookup, RequestLookup requestLookup, API api) {
+        Context context;
         if (executable.isPresent()) {
-            //TODO: set context for executable
-            Object jsOutput = executable.get().execute(Collections.emptyMap(), api);
+            Object executableOutput = executable.get().execute(getExecutableContext(requestLookup), api);
             if (log.isDebugEnabled()) {
-                log.debug("js ran produced output " + DebugUtil.safeJsonString(jsOutput));
+                log.debug("Executable output \"" + DebugUtil.safeJsonString(executableOutput) + "\".");
             }
-            if (jsOutput instanceof Map) {
-                //noinspection unchecked
-                model.combine((Map<String, Object>) jsOutput);
-            } else {
-                //TODO: is this necessary?
-                throw new UnsupportedOperationException();
-            }
+            context = Context.newContext(executableOutput).combine(getHbsModel(requestLookup));
+        } else {
+            context = Context.newContext(getHbsModel(requestLookup));
         }
-        ContextModel contextModel = ContextModel.from(model);
-        Context context = contextModel.getContext();
 
         context.data(DATA_KEY_LOOKUP, componentLookup);
         context.data(DATA_KEY_REQUEST_LOOKUP, requestLookup);
@@ -76,6 +66,14 @@ public class HbsPageRenderable extends HbsRenderable {
         String out = writer.toString(requestLookup.getPlaceholderContents());
         writer.close();
         return out;
+    }
+
+    protected Map<String, Object> getExecutableContext(RequestLookup requestLookup) {
+        Map<String, Object> context = new HashMap<>();
+        context.put("request", requestLookup.getRequest());
+        context.put("uriParams", requestLookup);
+        context.put("app", ImmutableMap.of("context", requestLookup.getAppContext(), "config", Collections.emptyMap()));
+        return context;
     }
 
     @Override
