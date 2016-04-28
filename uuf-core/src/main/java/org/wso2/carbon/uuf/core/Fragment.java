696803268731b7689b54c82899c135d0856ad953
sajithar <sajithar@wso2.com>
2016-04-28 16:32:29 +0530

    added 'getSimpleName' method to Fragment class

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/Fragment.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/Fragment.java
@@ -5,10 +5,16 @@ import org.wso2.carbon.uuf.model.Model;
 public class Fragment {
 
     private final String name;
+    private final String simpleName;
     private final Renderable renderer;
 
+    /**
+     * @param name     fully qualified name
+     * @param renderer renderer
+     */
     public Fragment(String name, Renderable renderer) {
         this.name = name;
+        this.simpleName = NameUtils.getSimpleName(name);
         this.renderer = renderer;
     }
 
@@ -16,15 +22,15 @@ public class Fragment {
         return name;
     }
 
+    public String getSimpleName() {
+        return simpleName;
+    }
+
     public String render(Model model, ComponentLookup componentLookup, RequestLookup requestLookup, API api) {
         requestLookup.pushToPublicUriStack(requestLookup.getAppContext() + componentLookup.getPublicUriInfix(this));
         String output = renderer.render(model, componentLookup, requestLookup, api);
         requestLookup.popPublicUriStack();
         return output;
-    }
-
-    public Renderable getRenderer() {
-        return renderer;
     }
 
     @Override
