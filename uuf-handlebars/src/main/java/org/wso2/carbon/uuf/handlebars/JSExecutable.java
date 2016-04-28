efd4e2be8b3a11dc5a37e2472a7ac431c758fc60
sajithar <sajithar@wso2.com>
2016-04-28 08:57:38 +0530

    integrated API class in JSExecutable class

--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/JSExecutable.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/JSExecutable.java
@@ -1,39 +1,37 @@
 package org.wso2.carbon.uuf.handlebars;
 
-import jdk.nashorn.api.scripting.AbstractJSObject;
+import jdk.nashorn.api.scripting.NashornScriptEngine;
 import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.wso2.carbon.uuf.core.API;
 import org.wso2.carbon.uuf.core.exception.UUFException;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
 import javax.script.ScriptException;
+import java.util.Map;
 import java.util.Optional;
 
 @SuppressWarnings("PackageAccessibility")
 public class JSExecutable implements Executable {
 
-    private static final NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+    private static final NashornScriptEngineFactory SCRIPT_ENGINE_FACTORY = new NashornScriptEngineFactory();
+    private static final String[] SCRIPT_ENGINE_ARGS = new String[]{"-strict"};
+
     private final Optional<String> scriptPath;
-    private final ScriptEngine engine;
-    private static final Logger log = LoggerFactory.getLogger(JSExecutable.class);
+    private final NashornScriptEngine engine;
 
     public JSExecutable(String scriptSource, ClassLoader componentClassLoader, Optional<String> scriptPath) {
         this.scriptPath = scriptPath;
         if (scriptPath.isPresent()) {
-            // Append script file name for debugging purposes
+            // Append script file name for debugging purposes.
             scriptSource = scriptSource + "//@ sourceURL=" + getPath();
         }
 
+        NashornScriptEngine engine = (NashornScriptEngine) SCRIPT_ENGINE_FACTORY.getScriptEngine(SCRIPT_ENGINE_ARGS,
+                                                                                                 componentClassLoader);
         try {
-            ScriptEngine engine = factory.getScriptEngine(new String[] { "-strict" }, componentClassLoader);
-            engine.eval("var callOSGiService = function(className, methodName, args){return API.callOSGiService(className, methodName, args)}");
             engine.eval(scriptSource);
             this.engine = engine;
         } catch (ScriptException e) {
-            throw new UUFException("error evaluating javascript", e);
+            throw new UUFException("An error occurred when evaluating the JavaScript file '" + getPath() + "'.", e);
         }
     }
 
@@ -42,33 +40,68 @@ public class JSExecutable implements Executable {
     }
 
     public Object execute(Object context, API api) {
-        Object rv;
+        engine.put("callOSGiService", (FunctionCallOSGiService) api::callOSGiService);
+        engine.put("getOSGiServices", (FunctionGetOSGiServices) api::getOSGiServices);
+        engine.put("callMicroService", (FunctionCallMicroService) api::callMicroService);
+        engine.put("createSession", (FunctionCreateSession) api::createSession);
+        engine.put("sendError", (FunctionSendError) api::sendError);
+        engine.put("sendRedirect", (FunctionSendRedirect) api::sendRedirect);
+        engine.put("setTheme", (FunctionSetTheme) api::setTheme);
         try {
-            engine.put("API", api);
-            rv = ((Invocable)engine).invokeFunction("onRequest", context);
+            return engine.invokeFunction("onRequest", context);
         } catch (ScriptException e) {
-            throw new UUFException("error while executing script " + getPath(), e);
+            throw new UUFException("An error occurred when executing the 'onRequest' function in JavaScript file '" +
+                                           getPath() + "' with context '" + context + "'.", e);
         } catch (NoSuchMethodException e) {
-            throw new UUFException("method 'onRequest' not defined in the script " + getPath(), e);
+            throw new UUFException("Cannot find the 'onRequest' function in the JavaScript file '" + getPath() + "'.",
+                                   e);
         }
-        return rv;
     }
 
     @Override
     public String toString() {
-        return "{path:'" + getPath() + "'}";
+        return "{\"path\": \"" + getPath() + "\"}";
     }
 
-    @Deprecated
-    private class MSSCaller extends AbstractJSObject {
-        @Override
-        public Object call(Object jsThis, Object... args) {
-            return "{}";
-        }
+    @FunctionalInterface
+    private interface FunctionCallOSGiService {
+        @SuppressWarnings("unused")
+        Object call(String serviceClassName, String serviceMethodName, Object... args);
+    }
 
-        @Override
-        public boolean isFunction() {
-            return true;
-        }
+    @FunctionalInterface
+    private interface FunctionGetOSGiServices {
+        @SuppressWarnings("unused")
+        Map<String, Object> call(String serviceClassName);
+    }
+
+    @FunctionalInterface
+    private interface FunctionCallMicroService {
+        @SuppressWarnings("unused")
+        void call();
+    }
+
+    @FunctionalInterface
+    private interface FunctionCreateSession {
+        @SuppressWarnings("unused")
+        void call();
+    }
+
+    @FunctionalInterface
+    private interface FunctionSendError {
+        @SuppressWarnings("unused")
+        void call(int status, String message);
+    }
+
+    @FunctionalInterface
+    private interface FunctionSendRedirect {
+        @SuppressWarnings("unused")
+        void call(String redirectUrl);
+    }
+
+    @FunctionalInterface
+    private interface FunctionSetTheme {
+        @SuppressWarnings("unused")
+        void call(String name);
     }
 }
