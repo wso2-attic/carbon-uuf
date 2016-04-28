00b492a1fa7342403a3008d6fa0ad40971a6f805
sajithar <sajithar@wso2.com>
2016-04-29 02:22:06 +0530

    changed the logic inside of the 'renderPage' method of Component class to get URI params at the same time

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/Component.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/Component.java
@@ -4,11 +4,7 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.wso2.carbon.uuf.model.MapModel;
-import org.wso2.carbon.uuf.model.Model;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -51,33 +47,34 @@ public class Component {
     }
 
     public Optional<String> renderPage(String pageUri, RequestLookup requestLookup, API api) {
-        Optional<Page> servingPage = getPage(pageUri);
-        if (!servingPage.isPresent()) {
+        Page servingPage = null;
+        for (Page page : pages) {
+            Optional<Map<String, String>> uriParams = page.getUriPatten().match(pageUri);
+            if (uriParams.isPresent()) {
+                requestLookup.setUriParams(uriParams.get());
+                servingPage = page;
+                break;
+            }
+        }
+        if (servingPage == null) {
             return Optional.<String>empty();
         }
 
-        Page page = servingPage.get();
         if (log.isDebugEnabled()) {
-            log.debug("Component '" + lookup.getComponentName() + "' is serving Page '" + page + "' for URI '" +
+            log.debug("Component '" + lookup.getComponentName() + "' is serving Page '" + servingPage + "' for URI '" +
                               pageUri + "'.");
         }
 
-        Model model = new MapModel(new HashMap<>());
-        return Optional.of(page.render(model, lookup, requestLookup, api));
-    }
-
-    private Optional<Page> getPage(String pageUri) {
-        return pages.stream().filter(page -> page.getUriPatten().matches(pageUri)).findFirst();
+        return Optional.of(servingPage.render(null, lookup, requestLookup, api));
     }
 
     @Deprecated
-    public boolean hasPage(String uri) {
-        return getPage(uri).isPresent();
+    public boolean hasPage(String pageUri) {
+        return pages.stream().filter(page -> page.getUriPatten().matches(pageUri)).findFirst().isPresent();
     }
 
-    @Deprecated
     public Set<Page> getPages() {
-        return new HashSet<>(pages);
+        return pages;
     }
 
     @Override
@@ -86,9 +83,10 @@ public class Component {
     }
 
     /**
-     * Returns a hash code value for the object. This method is supported for the benefit of hash tables such
-     * as those provided by {@link java.util.HashMap}.
-     * This assumes name, version and context is immutable (http://stackoverflow.com/a/27609/1560536).
+     * Returns a hash code value for the object. This method is supported for the benefit of hash tables such as those
+     * provided by {@link java.util.HashMap}. This assumes name, version and context is immutable
+     * (http://stackoverflow.com/a/27609/1560536).
+     *
      * @return a hash code value for this object.
      */
     @Override
@@ -98,15 +96,18 @@ public class Component {
 
     /**
      * Indicates whether some other object is "equal to" this one.
+     *
      * @param obj the reference object with which to compare.
      * @return {@code true} if this object is the same as the obj argument; {@code false} otherwise.
      */
     @Override
     public boolean equals(Object obj) {
-        if (!(obj instanceof Component))
+        if (!(obj instanceof Component)) {
             return false;
-        if (obj == this)
+        }
+        if (obj == this) {
             return true;
+        }
 
         Component comp = (Component) obj;
         return new EqualsBuilder().
