35a91ee44c8761913d893067c6e9ff781a0596b6
sajithar <sajithar@wso2.com>
2016-04-28 16:33:45 +0530

    used 'NameUtils' class in ComponentLookup class

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/ComponentLookup.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/ComponentLookup.java
@@ -15,9 +15,8 @@ import static org.wso2.carbon.uuf.fileio.StaticResolver.STATIC_RESOURCE_URI_PREF
 public class ComponentLookup {
 
     private final String componentName;
-    private final String fullyQualifiedNamePrefix;
     private final String componentContext;
-    private final Map<String, String> dependenciesContexts;
+    private final Map<String, String> allComponentsContexts;
     private final Map<String, Layout> layouts;
     private final SetMultimap<String, Fragment> bindings;
     private final Map<String, Fragment> fragments;
@@ -28,37 +27,29 @@ public class ComponentLookup {
             throw new IllegalArgumentException("Component name cannot be empty.");
         }
         this.componentName = componentName;
-        this.fullyQualifiedNamePrefix = componentName + ".";
         if (!componentContext.startsWith("/")) {
             throw new IllegalArgumentException("Context of a component must start with a '/'.");
         }
         this.componentContext = componentContext;
-        this.dependenciesContexts = new HashMap<>();
+        this.allComponentsContexts = new HashMap<>();
+        this.allComponentsContexts.put(componentName, componentContext);
 
-        this.layouts = layouts.stream().collect(Collectors.toMap(l -> fullyQualifiedName(l.getName()), l -> l));
+        this.layouts = layouts.stream().collect(Collectors.toMap(Layout::getName, l -> l));
 
-        this.fragments = fragments.stream().collect(Collectors.toMap(f -> fullyQualifiedName(f.getName()), f -> f));
+        this.fragments = fragments.stream().collect(Collectors.toMap(Fragment::getName, f -> f));
         this.bindings = HashMultimap.create();
         for (Map.Entry<String, Fragment> entry : bindings.entries()) {
-            this.bindings.put(fullyQualifiedName(entry.getKey()), entry.getValue());
+            this.bindings.put(NameUtils.getFullyQualifiedName(componentName, entry.getKey()), entry.getValue());
         }
 
         for (Component dependency : dependencies) {
             ComponentLookup dependencyLookup = dependency.getLookup();
-            this.dependenciesContexts.put(dependencyLookup.componentName, dependencyLookup.componentContext);
-            this.dependenciesContexts.putAll(dependencyLookup.dependenciesContexts);
+            this.allComponentsContexts.put(dependencyLookup.componentName, dependencyLookup.componentContext);
+            this.allComponentsContexts.putAll(dependencyLookup.allComponentsContexts);
             this.layouts.putAll(dependencyLookup.layouts);
             this.fragments.putAll(dependencyLookup.fragments);
             this.bindings.putAll(dependencyLookup.bindings);
         }
-    }
-
-    private String fullyQualifiedName(String name) {
-        return fullyQualifiedNamePrefix + name; // <component-name>.<binding/fragment-name>
-    }
-
-    private String getFullyQualifiedName(String name) {
-        return (name.indexOf('.') == -1) ? fullyQualifiedName(name) : name;
     }
 
     public String getComponentName() {
@@ -70,15 +61,15 @@ public class ComponentLookup {
     }
 
     public Optional<Layout> getLayout(String layoutName) {
-        return Optional.ofNullable(layouts.get(getFullyQualifiedName(layoutName)));
+        return Optional.ofNullable(layouts.get(NameUtils.getFullyQualifiedName(componentName, layoutName)));
     }
 
     public Set<Fragment> getBindings(String zoneName) {
-        return bindings.get(getFullyQualifiedName(zoneName));
+        return bindings.get(NameUtils.getFullyQualifiedName(componentName, zoneName));
     }
 
     public Optional<Fragment> getFragment(String fragmentName) {
-        return Optional.ofNullable(fragments.get(getFullyQualifiedName(fragmentName)));
+        return Optional.ofNullable(fragments.get(NameUtils.getFullyQualifiedName(componentName, fragmentName)));
     }
 
     Map<String, Fragment> getFragments() {
@@ -90,22 +81,13 @@ public class ComponentLookup {
     }
 
     String getPublicUriInfix(Layout layout) {
-        int lastDotIndex = layout.getName().lastIndexOf('.');
-        if (lastDotIndex == -1) {
-            return STATIC_RESOURCE_URI_PREFIX + componentContext + "/" + layout.getName();
-        } else {
-            String dependencyName = layout.getName().substring(0, lastDotIndex);
-            return STATIC_RESOURCE_URI_PREFIX + dependenciesContexts.get(dependencyName);
-        }
+        String componentName = NameUtils.getComponentName(layout.getName());
+        return STATIC_RESOURCE_URI_PREFIX + allComponentsContexts.get(componentName) + "/" +
+                DIR_NAME_COMPONENT_RESOURCES;
     }
 
     String getPublicUriInfix(Fragment fragment) {
-        int lastDotIndex = fragment.getName().lastIndexOf('.');
-        if (lastDotIndex == -1) {
-            return STATIC_RESOURCE_URI_PREFIX + componentContext + "/" + fragment.getName();
-        } else {
-            String dependencyName = fragment.getName().substring(0, lastDotIndex);
-            return STATIC_RESOURCE_URI_PREFIX + dependenciesContexts.get(dependencyName);
-        }
+        String componentName = NameUtils.getComponentName(fragment.getName());
+        return STATIC_RESOURCE_URI_PREFIX + allComponentsContexts.get(componentName) + "/" + fragment.getSimpleName();
     }
 }
