e6d2f7ac751dfe351c45a2686c8f42e5695f499b
sajithar <sajithar@wso2.com>
2016-04-28 16:32:19 +0530

    added 'getSimpleName' method to Layout class

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/Layout.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/Layout.java
@@ -1,16 +1,27 @@
 package org.wso2.carbon.uuf.core;
 
 public class Layout {
+
     private final String name;
+    private final String simpleName;
     private final Renderable renderer;
 
+    /**
+     * @param name     fully qualified name
+     * @param renderer renderer
+     */
     public Layout(String name, Renderable renderer) {
         this.name = name;
+        this.simpleName = NameUtils.getSimpleName(name);
         this.renderer = renderer;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getSimpleName() {
+        return simpleName;
     }
 
     public String render(ComponentLookup componentLookup, RequestLookup requestLookup) {
