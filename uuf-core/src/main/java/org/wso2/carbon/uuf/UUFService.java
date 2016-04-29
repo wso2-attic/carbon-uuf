594c35261fc63b5dd0384a4cb3596a30593adf32
sajithar <sajithar@wso2.com>
2016-04-29 16:38:00 +0530

    code formatting

--- uuf-core/src/main/java/org/wso2/carbon/uuf/UUFService.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/UUFService.java
@@ -21,7 +21,6 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.jndi.JNDIContextManager;
 import org.slf4j.MDC;
 import org.wso2.carbon.uuf.core.create.AppCreator;
 import org.wso2.carbon.uuf.core.create.RenderableCreator;
@@ -53,7 +52,6 @@ public class UUFService implements Microservice {
 
     private UUFRegistry registry;
     private final AtomicInteger count = new AtomicInteger(0);
-    private JNDIContextManager jndiContextManager;
 
     @SuppressWarnings("unused")
     public UUFService() {
