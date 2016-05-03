1b62c6b5391cc061f38a91cf9838cd555b972865
sajithar <sajithar@wso2.com>
2016-05-03 20:01:06 +0530

    added 'in' methods, 'out' method, implemented 'hashcode' & 'equals' methods changed resolving logic in 'getBindings', 'getFragments' method optimized computation in 'getPublicUriInfix' methods

--- uuf-core/src/main/java/org/wso2/carbon/uuf/core/ComponentLookup.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/core/ComponentLookup.java
@@ -19,12 +19,16 @@ package org.wso2.carbon.uuf.core;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.wso2.carbon.uuf.core.NameUtils.getFullyQualifiedName;
 import static org.wso2.carbon.uuf.fileio.StaticResolver.DIR_NAME_COMPONENT_RESOURCES;
 import static org.wso2.carbon.uuf.fileio.StaticResolver.STATIC_RESOURCE_URI_PREFIX;
 
@@ -32,10 +36,12 @@ public class ComponentLookup {
 
     private final String componentName;
     private final String componentContext;
-    private final Map<String, String> allComponentsContexts;
-    private final Map<String, Layout> layouts;
-    private final SetMultimap<String, Fragment> bindings;
-    private final Map<String, Fragment> fragments;
+    private final Map<String, Layout> allLayouts;
+    private final SetMultimap<String, Fragment> allBindings;
+    private final Map<String, Fragment> allFragments;
+    private final Map<String, String> allPublicUriPrefixes;
+    private final Map<String, ComponentLookup> immediateDependenciesLookups;
+    private final Deque<String> componentsStack;
 
     public ComponentLookup(String componentName, String componentContext, Set<Layout> layouts, Set<Fragment> fragments,
                            SetMultimap<String, Fragment> bindings, Set<Component> dependencies) {
@@ -47,25 +53,59 @@ public class ComponentLookup {
             throw new IllegalArgumentException("Context of a component must start with a '/'.");
         }
         this.componentContext = componentContext;
-        this.allComponentsContexts = new HashMap<>();
-        this.allComponentsContexts.put(componentName, componentContext);
 
-        this.layouts = layouts.stream().collect(Collectors.toMap(Layout::getName, l -> l));
-
-        this.fragments = fragments.stream().collect(Collectors.toMap(Fragment::getName, f -> f));
-        this.bindings = HashMultimap.create();
+        this.allLayouts = layouts.stream().collect(Collectors.toMap(Layout::getName, l -> l));
+        this.allFragments = fragments.stream().collect(Collectors.toMap(Fragment::getName, f -> f));
+        this.allBindings = HashMultimap.create();
         for (Map.Entry<String, Fragment> entry : bindings.entries()) {
-            this.bindings.put(NameUtils.getFullyQualifiedName(componentName, entry.getKey()), entry.getValue());
+            this.allBindings.put(getFullyQualifiedName(componentName, entry.getKey()), entry.getValue());
         }
+        this.allPublicUriPrefixes = new HashMap<>();
+        this.allPublicUriPrefixes.put(this.componentName, getPublicUriPrefix(this.componentContext));
+
+        this.immediateDependenciesLookups = new HashMap<>();
 
         for (Component dependency : dependencies) {
+            String dependencyComponentName = dependency.getName();
             ComponentLookup dependencyLookup = dependency.getLookup();
-            this.allComponentsContexts.put(dependencyLookup.componentName, dependencyLookup.componentContext);
-            this.allComponentsContexts.putAll(dependencyLookup.allComponentsContexts);
-            this.layouts.putAll(dependencyLookup.layouts);
-            this.fragments.putAll(dependencyLookup.fragments);
-            this.bindings.putAll(dependencyLookup.bindings);
+            this.immediateDependenciesLookups.put(dependencyComponentName, dependencyLookup);
+            this.allLayouts.putAll(dependencyLookup.allLayouts);
+            this.allFragments.putAll(dependencyLookup.allFragments);
+            this.allBindings.putAll(dependencyLookup.allBindings);
+            this.allPublicUriPrefixes.put(dependencyComponentName,
+                                          getPublicUriPrefix(dependencyLookup.componentContext));
+            this.allPublicUriPrefixes.putAll(dependencyLookup.allPublicUriPrefixes);
         }
+
+        this.componentsStack = new ArrayDeque<>(0);
+    }
+
+    private static String getPublicUriPrefix(String componentContext) {
+        return STATIC_RESOURCE_URI_PREFIX + componentContext + "/";
+    }
+
+    private Optional<ComponentLookup> getComponentLookup(String componentName) {
+        if (this.componentName.equals(componentName)) {
+            // Found in this lookup.
+            return Optional.of(this);
+        }
+
+        ComponentLookup immediateDependencyLookup = immediateDependenciesLookups.get(componentName);
+        if (immediateDependencyLookup != null) {
+            // Found in an immediate dependency.
+            return Optional.of(immediateDependencyLookup);
+        }
+
+        // Could not found in immediate dependencies. Hence, do a depth-first search in dependencies.
+        for (ComponentLookup dependencyLookup : immediateDependenciesLookups.values()) {
+            Optional<ComponentLookup> componentLookup = dependencyLookup.getComponentLookup(componentName);
+            if (componentLookup.isPresent()) {
+                return componentLookup;
+            }
+        }
+
+        // Could not find the lookup.
+        return Optional.<ComponentLookup>empty();
     }
 
     public String getComponentName() {
@@ -77,33 +117,72 @@ public class ComponentLookup {
     }
 
     public Optional<Layout> getLayout(String layoutName) {
-        return Optional.ofNullable(layouts.get(NameUtils.getFullyQualifiedName(componentName, layoutName)));
+        return Optional.ofNullable(allLayouts.get(NameUtils.getFullyQualifiedName(componentName, layoutName)));
     }
 
     public Set<Fragment> getBindings(String zoneName) {
-        return bindings.get(NameUtils.getFullyQualifiedName(componentName, zoneName));
+        String currentComponentName = getCurrentComponentName();
+        Optional<ComponentLookup> currentComponentLookup = getComponentLookup(currentComponentName);
+        if (currentComponentLookup.isPresent()) {
+            return currentComponentLookup.get().allBindings.get(getFullyQualifiedName(currentComponentName, zoneName));
+        } else {
+            return Collections.<Fragment>emptySet();
+        }
     }
 
     public Optional<Fragment> getFragment(String fragmentName) {
-        return Optional.ofNullable(fragments.get(NameUtils.getFullyQualifiedName(componentName, fragmentName)));
+        String currentComponentName = getCurrentComponentName();
+        return getComponentLookup(currentComponentName)
+                .map(lookup -> lookup.allFragments.get(getFullyQualifiedName(currentComponentName, fragmentName)));
     }
 
     Map<String, Fragment> getFragments() {
-        return fragments;
+        return allFragments;
     }
 
     String getPublicUriInfix(Page page) {
-        return STATIC_RESOURCE_URI_PREFIX + componentContext + "/" + DIR_NAME_COMPONENT_RESOURCES;
+        return allPublicUriPrefixes.get(getCurrentComponentName()) + DIR_NAME_COMPONENT_RESOURCES;
     }
 
     String getPublicUriInfix(Layout layout) {
-        String componentName = NameUtils.getComponentName(layout.getName());
-        return STATIC_RESOURCE_URI_PREFIX + allComponentsContexts.get(componentName) + "/" +
-                DIR_NAME_COMPONENT_RESOURCES;
+        return allPublicUriPrefixes.get(getCurrentComponentName()) + DIR_NAME_COMPONENT_RESOURCES;
     }
 
     String getPublicUriInfix(Fragment fragment) {
-        String componentName = NameUtils.getComponentName(fragment.getName());
-        return STATIC_RESOURCE_URI_PREFIX + allComponentsContexts.get(componentName) + "/" + fragment.getSimpleName();
+        return allPublicUriPrefixes.get(getCurrentComponentName()) + fragment.getSimpleName();
+    }
+
+    void in(Page page) {
+        componentsStack.addLast(this.componentName);
+    }
+
+    void in(Fragment fragment) {
+        componentsStack.addLast(NameUtils.getComponentName(fragment.getName()));
+    }
+
+    void in(Layout layout) {
+        componentsStack.addLast(NameUtils.getComponentName(layout.getName()));
+    }
+
+    private String getCurrentComponentName() {
+        return componentsStack.getLast();
+    }
+
+    String out() {
+        return componentsStack.removeLast();
+    }
+
+    @Override
+    public int hashCode() {
+        return componentName.hashCode() + (31 * componentContext.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj != null) && (obj instanceof ComponentLookup)) {
+            ComponentLookup other = (ComponentLookup) obj;
+            return componentName.equals(other.componentName) && componentContext.equals(other.componentContext);
+        }
+        return false;
     }
 }
