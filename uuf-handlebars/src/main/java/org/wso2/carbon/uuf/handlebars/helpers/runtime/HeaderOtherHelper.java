53fea047d421ecdfd92ca4fd8968d413202bb62b
sajithar <sajithar@wso2.com>
2016-05-06 18:05:45 +0530

    updated HeaderTitleHelper & HeaderOtherHelper classes by using Placeholder enum

--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/runtime/HeaderOtherHelper.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/runtime/HeaderOtherHelper.java
@@ -17,25 +17,22 @@
 package org.wso2.carbon.uuf.handlebars.helpers.runtime;
 
 import com.github.jknack.handlebars.Options;
-import org.wso2.carbon.uuf.core.RequestLookup;
+import org.wso2.carbon.uuf.core.Placeholder;
 import org.wso2.carbon.uuf.handlebars.helpers.FillPlaceholderHelper;
 
 import java.io.IOException;
-
-import static org.wso2.carbon.uuf.handlebars.HbsRenderable.DATA_KEY_REQUEST_LOOKUP;
 
 public class HeaderOtherHelper extends FillPlaceholderHelper {
 
     public static final String HELPER_NAME = "headerOther";
 
     public HeaderOtherHelper() {
-        super(HELPER_NAME);
+        super(Placeholder.HEAD_OTHER);
     }
 
     @Override
     public CharSequence apply(String s, Options options) throws IOException {
-        RequestLookup requestLookup = options.data(DATA_KEY_REQUEST_LOOKUP);
-        requestLookup.addToPlaceholder(placeholderName, options.fn().toString());
+        addToPlaceholder(options.fn().toString(), options);
         return "";
     }
 }
--- uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/runtime/HeaderTitleHelper.java
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/runtime/HeaderTitleHelper.java
@@ -17,29 +17,27 @@
 package org.wso2.carbon.uuf.handlebars.helpers.runtime;
 
 import com.github.jknack.handlebars.Options;
-import org.wso2.carbon.uuf.core.RequestLookup;
+import org.wso2.carbon.uuf.core.Placeholder;
 import org.wso2.carbon.uuf.handlebars.helpers.FillPlaceholderHelper;
 
 import java.io.IOException;
 import java.util.Optional;
 
-import static org.wso2.carbon.uuf.handlebars.HbsRenderable.DATA_KEY_REQUEST_LOOKUP;
-
 public class HeaderTitleHelper extends FillPlaceholderHelper {
     public static final String HELPER_NAME = "headerTitle";
 
     public HeaderTitleHelper() {
-        super(HELPER_NAME);
+        super(Placeholder.TITLE);
     }
 
     public CharSequence apply(String title, Options options) throws IOException {
-        RequestLookup requestLookup = options.data(DATA_KEY_REQUEST_LOOKUP);
-        Optional<String> currentTitle = requestLookup.getPlaceholderContent(placeholderName);
+        Optional<String> currentTitle = getPlaceholderValue(options);
         if (currentTitle.isPresent()) {
             throw new IllegalStateException(
                     "Cannot set header title. It is already set to '" + currentTitle.get() + "'.");
+        } else {
+            addToPlaceholder(title, options);
+            return "";
         }
-        requestLookup.addToPlaceholder(HELPER_NAME, title);
-        return "";
     }
 }
