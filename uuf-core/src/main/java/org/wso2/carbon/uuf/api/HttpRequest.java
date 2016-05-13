1b7a220e36a24ca0cab9244aa7e9f0ad1ab5255b
sajithar <sajithar@wso2.com>
2016-05-13 21:34:56 +0530

    changed HttpRequest class to a interface

--- uuf-core/src/main/java/org/wso2/carbon/uuf/api/HttpRequest.java
+++ uuf-core/src/main/java/org/wso2/carbon/uuf/api/HttpRequest.java
@@ -16,144 +16,62 @@
 
 package org.wso2.carbon.uuf.api;
 
-import io.netty.handler.codec.http.QueryStringDecoder;
-import org.wso2.carbon.uuf.internal.util.RequestUtil;
-import javax.ws.rs.core.HttpHeaders;
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * Provides a common interface to provide request information.
  */
-public class HttpRequest {
-
-    private final String method;
-    private final String protocol;
-    private final String queryString;
-    private final byte[] content;
-    private final String contentType;
-    private final long contentLength;
-    private final String url;
-    private final String uri;
-    private final String appContext;
-    private final String uriWithoutAppContext;
-    private final boolean isSecure;
-    private final String remoteAddr;
-    private final String contextPath;
-    private final int localPort;
-    private final InputStream inputStream;
-    private final Map<String, String> headers;
-    //TODO: Following need to be implemented.
-    //getPathTranslated
-    //getHeader
-    //getAllHeaders
-    //getParameter
-    //getAllParameters
-    //getLocale
-    //getAllLocales
-    //getMappedPath
-    //getFile(formFeildName)
-    //getAllFiles()
-    //getCookie(name)
-    //getAllCookies
-
-    /**
-     * Constructs a HTTP request using netty http request.
-     *
-     * @param request
-     */
-    public HttpRequest(io.netty.handler.codec.http.HttpRequest request, byte[] content) {
-        this.method = request.getMethod().name();
-        this.protocol = request.getProtocolVersion().text();
-        this.queryString = QueryStringDecoder.decodeComponent(request.getUri());
-        if (content != null) {
-            this.content = content;
-            this.contentLength = content.length;
-            this.inputStream = new ByteArrayInputStream(content);
-        } else {
-            this.content = null;
-            this.contentLength = 0;
-            this.inputStream = null;
-        }
-        //
-        this.contentType = request.headers().get(HttpHeaders.CONTENT_TYPE);
-        this.uri = request.getUri();
-        this.appContext = RequestUtil.getAppContext(this.uri);
-        this.uriWithoutAppContext = RequestUtil.getUriWithoutAppContext(this.uri);
-        this.headers = request.headers().entries().stream().collect(Collectors.toMap(Map.Entry::getKey,
-                Map.Entry::getValue));
-        //not implemented yet
-        this.url = null;
-        this.isSecure = false;
-        this.remoteAddr = null;
-        this.contextPath = null;
-        this.localPort = -1;
-    }
+public interface HttpRequest {
 
     /**
      * Returns the method of the request
      *
      * @return http method
      */
-    public String getMethod() {
-        return method;
-    }
+    String getMethod();
 
     /**
      * Returns the protocol of the request
      *
      * @return protocol
      */
-    public String getProtocol() {
-        return protocol;
-    }
+    String getProtocol();
 
     /**
      * Returns the query String of the request
      *
      * @return query string
      */
-    public String getQueryString() {
-        return queryString;
-    }
+    String getQueryString();
 
     /**
      * Returns the content of the request as string.
      *
      * @return converted content string
      */
-    public String getContent() {
-        return new String(content);
-    }
+    String getContent();
 
     /**
      * Returns the content of the request as array of bytes.
      *
      * @return array of bytes
      */
-    public byte[] getContentBytes() {
-        return content;
-    }
+    byte[] getContentBytes();
 
     /**
      * Returns the content Type of the request.
      *
      * @return content type
      */
-    public String getContentType() {
-        return contentType;
-    }
+    String getContentType();
 
     /**
      * Returns the content length of the request.
      *
      * @return content length
      */
-    public long getContentLength() {
-        return contentLength;
-    }
+    long getContentLength();
 
     /**
      * Returns the part of this request's URL from the protocol name up to the query String in the first line of the
@@ -161,94 +79,65 @@ public class HttpRequest {
      *
      * @return request uri
      */
-    public String getUri() {
-        return uri;
-    }
+    String getUri();
 
-    public String getAppContext() {
-        return appContext;
-    }
+    String getAppContext();
 
-    public String getUriWithoutAppContext() {
-        return uriWithoutAppContext;
-    }
+    String getUriWithoutAppContext();
 
     /**
      * Reconstructs the URL the client used to make the request.
      *
      * @return request url
      */
-    public String getUrl() {
-        throw new UnsupportedOperationException();
-    }
+    String getUrl();
 
     /**
      * Returns true when https is used.
      *
      * @return true when https, false otherwise
      */
-    public boolean isSecure() {
-        throw new UnsupportedOperationException();
-    }
+    boolean isSecure();
 
     /**
      * Returns the Internet Protocol (IP) address of the client that sent the request.
      *
      * @return client ip address
      */
-    public String getRemoteAddr() {
-        throw new UnsupportedOperationException();
-    }
+    String getRemoteAddr();
 
     /**
      * Returns the portion of the request URI that indicates the context of the request. eg. /uuf-app
      *
      * @return context path
      */
-    public String getContextPath() {
-        throw new UnsupportedOperationException();
-    }
+    String getContextPath();
 
     /**
      * Returns the request local port number.
      *
      * @return port number
      */
-    public int getLocalPort() {
-        throw new UnsupportedOperationException();
-    }
+    int getLocalPort();
 
     /**
      * Retrieves the body of the request as binary data.
      *
      * @return
      */
-    public InputStream getInputStream() {
-        return inputStream;
-    }
+    InputStream getInputStream();
 
     /**
      * Retrieves the map of headers.
      *
      * @return
      */
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
+    Map<String, String> getHeaders();
 
     /**
      * Returns hostname. If host header is not found, returns //localhost
      *
      * @return
      */
-    public String getHostName() {
-        String hostHeader = headers.get(HttpHeaders.HOST);
-        return "//" + ((hostHeader == null) ? "localhost" : hostHeader);
-    }
-
-    @Override
-    public String toString() {
-        return "{\"method\": \"" + method + "\", \"url\": \"" + url + "\", \"protocol\": \"" + protocol +
-                "\", \"host\": \"" + getHostName() + "\"}";
-    }
+    String getHostName();
 }
