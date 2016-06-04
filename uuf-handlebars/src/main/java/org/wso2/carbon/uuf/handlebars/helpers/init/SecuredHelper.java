a25e39d80f11e00bf9c29d9f563d0a561479546f
sajithar <sajithar@wso2.com>
2016-06-04 08:35:21 +0530

    created init helper SecuredHelper class

---
+++ uuf-handlebars/src/main/java/org/wso2/carbon/uuf/handlebars/helpers/init/SecuredHelper.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wso2.carbon.uuf.handlebars.helpers.init;
+
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import org.wso2.carbon.uuf.handlebars.HbsPagePreprocessor;
+
+import java.io.IOException;
+
+public class SecuredHelper implements Helper<Object> {
+
+    public static final String HELPER_NAME = "secured";
+
+    @Override
+    public CharSequence apply(Object context, Options options) throws IOException {
+        if (options.tagType.inline()) {
+            // Is a {{secured}} inline helper, not the block version.
+            options.data(HbsPagePreprocessor.DATA_KEY_IS_SECURED, Boolean.TRUE);
+        }
+        return "";
+    }
+}
