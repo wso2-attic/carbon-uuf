4bc4a1e14ad90b03b685c3e47c6355db54364c92
prabushi <prabushisamarakoon@gmail.com>
2016-07-18 13:02:52 +0530

    added register page

--- samples/components/org.wso2.carbon.uuf.sample.basicauth/pages/login.js
+++ samples/components/org.wso2.carbon.uuf.sample.basicauth/pages/login.js
@@ -14,18 +14,31 @@
  *  limitations under the License.
  */
 
-
 function onRequest(env) {
-    if (env.request.method == "POST") {
-        // TODO: 5/18/16 read HTML form parameters & receive the username & password.
-        var username = "admin", password = "admin";
-        // TODO: 5/18/16 authenticate username & password.
-        try {
-            createSession(username);
-            sendRedirect(encodeURIComponent(env.app.env + "/"));
-        } catch (e) {
-            print(e);
+    var session = getSession();
+    if (session) {
+        sendRedirect(env.contextPath + env.config['loginRedirectUri']);
+    }
+    if (env.request.method === "POST") {
+        var username = env.request.formParams['username'];
+        var password = env.request.formParams['password'];
+        // calling dummy authenticate service
+        var result = authenticate(username, password);
+        if (result.success) {
+            //configure login redirect uri
+            //sendRedirect(env.contextPath + env.config['loginRedirectUri'] + '?username=' + username);
+            sendRedirect(env.contextPath + env.config['loginRedirectUri']);
+        } else {
+            // {success: true, message: ""};
+            return {login: result};
         }
     }
+}
 
+function authenticate(username, password) {
+    if (username === "admin" && password === "admin") {
+        createSession(username);
+        return {success: true, message: "success"}
+    }
+    return {success: false, message: "Incorrect username and password combination."};
 }
---
+++ samples/components/org.wso2.carbon.uuf.sample.basicauth/pages/register.js
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+function onRequest(env) {
+    if (env.request.method === "POST") {
+        var password = env.request.formParams['input-password'];
+        var confirmPassword = env.request.formParams['input-confirm-password'];
+        var result = validateData(password, confirmPassword);
+        if(result.success) {
+            sendRedirect(env.contextPath + env.config['registerRedirectUri']);
+        } else {
+            return {register: result};
+        }
+    }
+}
+
+function validateData(password, confirmPassowrd) {
+    if(password === '') {
+        return {success: false, message: "Please enter a password to continue registration."};
+    } else if(password !== confirmPassowrd){
+        return {success: false, message: "Incorrect password confirmation."};
+    }
+    return {success: true, message: "success"}
+}
