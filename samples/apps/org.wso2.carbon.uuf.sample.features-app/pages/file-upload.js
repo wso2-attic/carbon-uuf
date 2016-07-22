5f52c29c0922435cab8176d1c0c77161d551071e
prabushi <prabushisamarakoon@gmail.com>
2016-07-22 17:47:50 +0530

    added copying uploaded file into tmp directory

--- samples/apps/org.wso2.carbon.uuf.sample.features-app/pages/file-upload.js
+++ samples/apps/org.wso2.carbon.uuf.sample.features-app/pages/file-upload.js
@@ -16,25 +16,23 @@
 
 function onRequest(env) {
     if (env.request.method == "POST") {
-        var x = env.request.formParams['file-name'];
-        var y = env.request.files['file-name'];
-        var txt = "";
-        if (x.type === 'String' || x.type ==='files') {
-            //     if (x.files.length == 0) {
-            //         txt = "Select one or more files.";
-            //     } else {
-            //         // for (var i = 0; i < x.files.length; i++) {
-            //         //     txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-            //         //     var file = x.files[i];
-            //         //     if ('name' in file) {
-            //         //         txt += "name: " + file.name + "<br>";
-            //         //     }
-            //         //     if ('size' in file) {
-            //         //         txt += "size: " + file.size + " bytes <br>";
-            //         //     }
-            //         // }
-            txt= "file temp";
-        }
-        return {message: 'You have successfully uploaded the file,' + x+ '. ' + txt};
+        var Paths = Java.type('java.nio.file.Paths');
+        var System = Java.type('java.lang.System');
+        var Files = Java.type('java.nio.file.Files');
+        var StandardCopyOption = Java.type('java.nio.file.StandardCopyOption');
+
+        var uploadedFile = env.request.files["file-content"];
+        var tempDirPath = System.getProperty('java.io.tmpdir');
+        var destination = Paths.get(tempDirPath).resolve(uploadedFile.name);
+        var sourcePath = Paths.get(uploadedFile.path);
+        var destinationPath = Paths.get(destination);
+
+        //copy the selected file to UUF_HOME/tmp directory overriding any existing content with the same name
+        Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+        return {
+            message: 'You have successfully uploaded the file, ' + uploadedFile.name + ' and copied it to '
+                     + tempDirPath + ' directory.'
+        };
     }
 }
