a92ffe7dd55428f5173469300faf6df47d369689
Sajith Janaprasad <sajithar@wso2.com>
2016-09-16 19:25:32 +0530

    fixed the license header in 'uuf.api.auth' package

--- components/uuf-core/src/main/java/org/wso2/carbon/uuf/api/auth/Session.java
+++ components/uuf-core/src/main/java/org/wso2/carbon/uuf/api/auth/Session.java
@@ -1,29 +1,31 @@
 /*
  *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
 
 package org.wso2.carbon.uuf.api.auth;
 
 import org.wso2.carbon.uuf.core.Theme;
+import org.wso2.carbon.uuf.spi.auth.User;
 
 import java.io.Serializable;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
-import org.wso2.carbon.uuf.spi.auth.User;
 
 /**
  * Provides a way to identify a user across more than one page request or visit to a Web site and to store information
