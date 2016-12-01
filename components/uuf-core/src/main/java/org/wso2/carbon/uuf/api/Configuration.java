3a7785a6be40a1e208ae513db88861e0a323e8c4
Sajith Janaprasad <sajithar@wso2.com>
2016-12-01 10:28:09 +0530

    removed the default constructor, 'merge' method in Configuration class

--- components/uuf-core/src/main/java/org/wso2/carbon/uuf/api/Configuration.java
+++ components/uuf-core/src/main/java/org/wso2/carbon/uuf/api/Configuration.java
@@ -21,8 +21,6 @@ package org.wso2.carbon.uuf.api;
 import org.wso2.carbon.uuf.exception.InvalidTypeException;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -38,8 +36,8 @@ public class Configuration {
     private static final String KEY_MENU = "menu";
     private static final String KEY_ERROR_PAGES = "errorPages";
 
-    private final Map<String, Object> map = new HashMap<>();
-    private final Map<String, Object> unmodifiableMap = Collections.unmodifiableMap(map);
+    private final Map<String, Object> map;
+    private final Map<String, Object> unmodifiableMap;
     // We cache following values to avoid extracting them from 'map' and validating them over and over again.
     private String cachedContextPath;
     private String cachedThemeName;
@@ -48,11 +46,17 @@ public class Configuration {
     private Map<Integer, String> cachedErrorPageUris = new ConcurrentHashMap<>();
     private String cachedDefaultErrorPageUri;
 
-    public Configuration() {
-    }
-
+    @SuppressWarnings("unchecked")
     public Configuration(Map<?, ?> rawMap) {
-        merge(rawMap);
+        // Validation
+        rawMap.keySet().forEach(key -> {
+            if (!(key instanceof String)) {
+                throw new InvalidTypeException("Configurations must be a Map<String, Object>. Instead found a '" +
+                                                       key.getClass().getName() + "' key.");
+            }
+        });
+        this.map = (Map<String, Object>) rawMap;
+        this.unmodifiableMap = Collections.unmodifiableMap(map);
     }
 
     public Optional<String> getContextPath() {
@@ -256,66 +260,6 @@ public class Configuration {
         }
         cachedDefaultErrorPageUri = defaultErrorPageUri; // Cache computed value.
         return Optional.of(defaultErrorPageUri);
-    }
-
-    public void merge(Map<?, ?> rawMap) {
-        // TODO: 7/7/16 lock configuration after deploying the app
-        for (Map.Entry<?, ?> entry : rawMap.entrySet()) {
-            if (entry.getKey() instanceof String) {
-                map.compute((String) entry.getKey(), (key, oldValue) -> {
-                    Object newValue = entry.getValue();
-                    if (oldValue == null) {
-                        return newValue; // Add the new value.
-                    }
-
-                    if (newValue instanceof Map && oldValue instanceof Map) {
-                        return deepMergeMap((Map) oldValue, (Map) newValue);
-                    } else if (newValue instanceof List && oldValue instanceof List) {
-                        return deepMergeList((List) oldValue, (List) newValue);
-                    }
-                    return newValue; // Cannot merge if not a Map or a List, hence replace with the new value.
-                });
-            } else {
-                throw new InvalidTypeException("Configurations must be a Map<String, Object>. Instead found a '" +
-                                                       entry.getKey().getClass().getName() + "' key.");
-            }
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private Map deepMergeMap(Map oldMap, Map newMap) {
-        for (Object key : newMap.keySet()) {
-            Object newValueObj = newMap.get(key);
-            Object oldValueObj = oldMap.get(key);
-            if (oldValueObj instanceof Map && newValueObj instanceof Map) {
-                oldMap.put(key, deepMergeMap((Map) oldValueObj, (Map) newValueObj));
-            } else if (oldValueObj instanceof List && newValueObj instanceof List) {
-                oldMap.put(key, deepMergeList((List) oldValueObj, (List) newValueObj));
-            } else {
-                oldMap.put(key, newValueObj);
-            }
-        }
-        return oldMap;
-    }
-
-    @SuppressWarnings("unchecked")
-    private List deepMergeList(List oldList, List newList) {
-        for (Object newItemObj : newList) {
-            int oldIndex = oldList.indexOf(newItemObj);
-            if (oldIndex != -1) {
-                Object oldItemObj = oldList.get(oldIndex);
-                if (oldItemObj instanceof List && newItemObj instanceof List) {
-                    oldList.set(oldIndex, deepMergeList((List) oldItemObj, (List) newItemObj));
-                } else if (oldItemObj instanceof Map && newItemObj instanceof Map) {
-                    oldList.set(oldIndex, deepMergeMap((Map) oldItemObj, (Map) newItemObj));
-                } else {
-                    oldList.set(oldIndex, newItemObj);
-                }
-            } else {
-                oldList.add(newItemObj);
-            }
-        }
-        return oldList;
     }
 
     /**
