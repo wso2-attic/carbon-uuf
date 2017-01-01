048154a911e2ff3d63a64c80e67f6f8a159c8a72
Sajith Janaprasad <sajithar@wso2.com>
2017-01-02 00:44:22 +0530

    fixed APITest test case

--- components/uuf-core/src/test/java/org/wso2/carbon/uuf/APITest.java
+++ components/uuf-core/src/test/java/org/wso2/carbon/uuf/APITest.java
@@ -23,76 +23,81 @@ import org.wso2.carbon.uuf.api.reference.ComponentReference;
 import org.wso2.carbon.uuf.api.reference.FileReference;
 import org.wso2.carbon.uuf.internal.deployment.AppCreator;
 import org.wso2.carbon.uuf.internal.deployment.ClassLoaderProvider;
-import org.wso2.carbon.uuf.spi.RenderableCreator;
 
+import java.util.Collections;
 import java.util.Dictionary;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * This test class contains test method(s) to test the API deployment capability feature of UUF.
  */
 public class APITest {
 
-    @SuppressWarnings({"unchecked"})
     @Test
     public void testAPIContextPath() throws ClassNotFoundException {
-        String appContextpath = "/sample-app";
+        String appContextPath = "/sample-app";
         String apiClassName = "org.wso2.carbon.uuf.SampleAPI";
-        String apiURI = "/sample-api";
+        String apiUri = "/sample-api";
 
-        RenderableCreator renderableCreator = mock(RenderableCreator.class);
-        when(renderableCreator.getSupportedFileExtensions()).thenReturn(new HashSet());
-        Set<RenderableCreator> renderableCreators = new HashSet<>();
-        renderableCreators.add(renderableCreator);
+        // Create root component reference.
+        ComponentReference rootComponentReference = mock(ComponentReference.class);
+        when(rootComponentReference.getLayouts(any())).thenReturn(Stream.empty());
+        when(rootComponentReference.getFragments(any())).thenReturn(Stream.empty());
+        when(rootComponentReference.getPages(any())).thenReturn(Stream.empty());
+        FileReference componentConfigurationFileReference = mock(FileReference.class);
+        when(componentConfigurationFileReference.getContent())
+                .thenReturn("# testing\n" +
+                                    "apis:\n" +
+                                    "  - className: " + apiClassName + "\n" +
+                                    "    uri: " + apiUri + "\n" +
+                                    "bindings: null\n" +
+                                    "config: null");
+        when(rootComponentReference.getConfiguration()).thenReturn(componentConfigurationFileReference);
 
-        FileReference dependencyTreeFileReference = mock(FileReference.class);
-        FileReference configFileReference = mock(FileReference.class);
-        FileReference manifestFileReference = mock(FileReference.class);
-        when(dependencyTreeFileReference.getContent()).thenReturn(
-                "artifactId: org.wso2.carbon.uuf.sample.sample-app.feature\n" +
-                        "version: 1.0.0-SNAPSHOT\n" +
-                        "contextPath: " + appContextpath);
-        when(manifestFileReference.getContent()).thenReturn(
-                "apis:\n" +
-                        "    - className: \"" + apiClassName + "\"\n" +
-                        "      uri: \"" + apiURI + "\"");
-        when(configFileReference.getContent()).thenReturn("appName: Sample Application");
-
-        ComponentReference componentReference = mock(ComponentReference.class);
-        when(componentReference.getLayouts(any())).thenReturn(Stream.empty());
-        when(componentReference.getFragments(any())).thenReturn(Stream.empty());
-        when(componentReference.getPages(any())).thenReturn(Stream.empty());
-        when(componentReference.getManifest()).thenReturn(Optional.of(manifestFileReference));
-
+        // Create app reference.
         AppReference appReference = mock(AppReference.class);
-        when(appReference.getConfiguration()).thenReturn(configFileReference);
-        when(appReference.getDependencyTree()).thenReturn(dependencyTreeFileReference);
         when(appReference.getThemeReferences()).thenReturn(Stream.empty());
-        when(appReference.getComponentReference(any())).thenReturn(componentReference);
+        when(appReference.getComponentReference(eq("/root"))).thenReturn(rootComponentReference);
+        FileReference appConfigurationFileReference = mock(FileReference.class);
+        when(appConfigurationFileReference.getContent())
+                .thenReturn("# testing\n" +
+                                    "contextPath: null\n" +
+                                    "theme: null\n" +
+                                    "errorPages: null\n" +
+                                    "menus: null\n" +
+                                    "other: null\n" +
+                                    "security: null");
+        when(appReference.getConfiguration()).thenReturn(appConfigurationFileReference);
+        FileReference dependencyTreeFileReference = mock(FileReference.class);
+        when(dependencyTreeFileReference.getContent())
+                .thenReturn("# testing\n" +
+                                    "artifactId: org.wso2.carbon.uuf.sample.sample-app.feature\n" +
+                                    "version: 1.0.0-SNAPSHOT\n" +
+                                    "contextPath: " + appContextPath);
+        when(appReference.getDependencyTree()).thenReturn(dependencyTreeFileReference);
 
+        // Create class loader provider.
         ClassLoaderProvider classLoaderProvider = mock(ClassLoaderProvider.class);
-        ClassLoader classLoader = mock(ClassLoader.class);
-        when(classLoaderProvider.getClassLoader(any(), any(), any())).thenReturn(classLoader);
-        when(classLoader.loadClass(apiClassName)).thenReturn((Class) SampleAPI.class);
-
-        // This single element array is used to store the api context path
-        final String[] apiContextPath = new String[1];
+        when(classLoaderProvider.getClassLoader(any(), any(), any())).thenReturn(ClassLoader.getSystemClassLoader());
+        String[] apiContextPath = new String[1]; // variable used in a lambda should be final or effectively final
         doAnswer(invocation -> {
+            @SuppressWarnings("unchecked")
             Dictionary<String, String> serviceProperties = (Dictionary<String, String>) invocation.getArguments()[1];
             apiContextPath[0] = serviceProperties.get("contextPath");
             return null;
         }).when(classLoaderProvider).deployAPI(any(), any());
 
-        AppCreator appCreator = new AppCreator(renderableCreators, classLoaderProvider);
-        appCreator.createApp(appReference, appContextpath);
+        // Create app creator.
+        AppCreator appCreator = new AppCreator(Collections.emptySet(), classLoaderProvider);
+        appCreator.createApp(appReference, appContextPath);
 
-        String expectedApiContextPath = appContextpath + "/root/apis" + apiURI;
+        String expectedApiContextPath = appContextPath + "/root/apis" + apiUri;
         Assert.assertEquals(apiContextPath[0], expectedApiContextPath, "Calculated API context path is wrong.");
     }
 
