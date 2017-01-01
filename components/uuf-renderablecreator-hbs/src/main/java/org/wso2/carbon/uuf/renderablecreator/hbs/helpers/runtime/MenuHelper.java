8c375a17949ef939d03a3ec41c019882a99a3a82
Sajith Janaprasad <sajithar@wso2.com>
2017-01-02 01:25:46 +0530

    fixed MenuHelper

--- components/uuf-renderablecreator-hbs/src/main/java/org/wso2/carbon/uuf/renderablecreator/hbs/helpers/runtime/MenuHelper.java
+++ components/uuf-renderablecreator-hbs/src/main/java/org/wso2/carbon/uuf/renderablecreator/hbs/helpers/runtime/MenuHelper.java
@@ -17,73 +17,59 @@
 package org.wso2.carbon.uuf.renderablecreator.hbs.helpers.runtime;
 
 import com.github.jknack.handlebars.Context;
+import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
+import org.wso2.carbon.uuf.api.config.Configuration;
 import org.wso2.carbon.uuf.core.Lookup;
 import org.wso2.carbon.uuf.renderablecreator.hbs.core.HbsRenderable;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 
 public class MenuHelper implements Helper<Object> {
 
     public static final String HELPER_NAME = "menu";
-    private static final String KEY_THIS = "this";
 
     @Override
     @SuppressWarnings("unchecked")
     public CharSequence apply(Object context, Options options) throws IOException {
-        if (context == null) {
+        if (Handlebars.Utils.isEmpty(context)) {
             return "";
         }
 
-        Map<String, Map> menu;
+        List<Configuration.MenuItem> menuItems;
         if (context instanceof String) {
             String menuName = (String) context;
-            if (menuName.isEmpty()) {
-                throw new IllegalArgumentException("Invalid menu name. Menu name cannot be empty.");
-            }
             Lookup lookup = options.data(HbsRenderable.DATA_KEY_LOOKUP);
-            menu = lookup.getConfiguration().getMenu(menuName);
-            if (menu == null) {
-                return ""; // Menu 'menuName' does not exists.
-            }
-        } else if (context instanceof Map) {
-            Map<?, ?> rawMap = (Map) context;
-            for (Map.Entry<?, ?> entry : rawMap.entrySet()) {
-                if (!(entry.getKey() instanceof String)) {
-                    throw new IllegalArgumentException("Expected a Map<String, Map> context. Instead found a '" +
-                                                               entry.getKey().getClass().getName() + "' key.");
-                } else if (!(entry.getValue() instanceof Map)) {
-                    throw new IllegalArgumentException("Expected a Map<String, Map> context. Instead found a '" +
-                                                               entry.getValue().getClass().getName() + "' value at '" +
-                                                               entry.getKey() + "' key.");
+            menuItems = lookup.getConfiguration().getMenu(menuName);
+        } else if (context instanceof Configuration.MenuItem[]) {
+            menuItems = Arrays.asList((Configuration.MenuItem[]) context);
+        } else if (context instanceof List) {
+            List<?> rawList = (List) context;
+            for (Object item : rawList) {
+                if (!(item instanceof Configuration.MenuItem)) {
+                    throw new IllegalArgumentException(
+                            "List parsed as the context of the menu helper must be a List<Configuration.MenuItem>. " +
+                                    "Instead found a '" + context.getClass().getName() + "'.");
                 }
             }
-            menu = (Map<String, Map>) rawMap;
+            menuItems = (List<Configuration.MenuItem>) rawList;
         } else {
             throw new IllegalArgumentException(
-                    "Menu helper context must be either a string (menu name) or a Map<String, Map>. Instead found a '" +
-                            context.getClass().getName() + "' context.");
-        }
-        if (menu.isEmpty()) {
-            return "";
+                    "Menu helper context must be either a string (menu name) or a Configuration.MenuItem[] or a " +
+                            "List<Configuration.MenuItem>. Instead found a '" + context.getClass().getName() + "'.");
         }
 
-        Iterator<Map.Entry<String, Map>> iterator = menu.entrySet().iterator();
+        Iterator<Configuration.MenuItem> iterator = menuItems.iterator();
         Context parentContext = options.context;
-        Map valueOfThisKey = menu.get(KEY_THIS);
         boolean isFirstIteration = true;
         while (iterator.hasNext()) {
-            Map.Entry<String, Map> entry = iterator.next();
-            if (entry.getKey().equals(KEY_THIS)) {
-                continue; // Skip the entry with "@this" key.
-            }
-            boolean isLeaf = isLeafMenu(entry.getValue());
-            Context iterationContext = Context.newContext(parentContext, entry.getValue())
-                    .combine("@key", entry.getKey())
-                    .combine("@this", valueOfThisKey)
+            Configuration.MenuItem menuItem = iterator.next();
+            boolean isLeaf = menuItem.getSubmenus().isEmpty();
+            Context iterationContext = Context.newContext(parentContext, menuItem)
                     .combine("@first", isFirstIteration)
                     .combine("@last", !iterator.hasNext())
                     .combine("@leaf", isLeaf)
@@ -92,14 +78,5 @@ public class MenuHelper implements Helper<Object> {
             isFirstIteration = false;
         }
         return options.buffer();
-    }
-
-    private static boolean isLeafMenu(Map menu) {
-        for (Object value : menu.values()) {
-            if (!(value instanceof String)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
