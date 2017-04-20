d9d36bbd3502548c925e475437b2e8badc52d952
Sajith Janaprasad <sajithar@wso2.com>
2017-04-20 14:29:59 +0530

    fix javadoc in AppReference interface

--- components/uuf-core/src/main/java/org/wso2/carbon/uuf/api/reference/AppReference.java
+++ components/uuf-core/src/main/java/org/wso2/carbon/uuf/api/reference/AppReference.java
@@ -20,23 +20,76 @@ package org.wso2.carbon.uuf.api.reference;
 
 import java.util.stream.Stream;
 
+/**
+ * A reference to an UUF app artifact.
+ *
+ * @since 1.0.0
+ */
 public interface AppReference {
 
+    /**
+     * Name of the directory that contains UUF components.
+     */
     String DIR_NAME_COMPONENTS = "components";
+    /**
+     * Name of the directory that contains customized UUF components.
+     */
     String DIR_NAME_CUSTOMIZATIONS = "customizations";
+    /**
+     * Name of the directory that contains UUF themes.
+     */
     String DIR_NAME_THEMES = "themes";
+    /**
+     * Name of the file that has the dependency tree of the UUF components.
+     */
     String FILE_NAME_DEPENDENCY_TREE = "dependency-tree.yaml";
+    /**
+     * Name of the file that has the app configurations.
+     */
     String FILE_NAME_CONFIGURATION = "configuration.yaml";
 
+    /**
+     * Returns the name of the UUF app represented by this reference.
+     *
+     * @return name of the UUF app
+     */
     String getName();
 
+    /**
+     * Returns a reference to the UUF component that belongs to the UUF app represented by this reference.
+     *
+     * @param componentContext context of the UUF component
+     * @return reference to the UUF component
+     */
     ComponentReference getComponentReference(String componentContext);
 
+    /**
+     * Returns UUF theme reference of the UUF app represented by this reference.
+     *
+     * @return references for UUF themes of this UUF app
+     */
     Stream<ThemeReference> getThemeReferences();
 
+    /**
+     * Returns a reference to the dependency tree file of the UUF app represented by this reference.
+     *
+     * @return reference to the dependency tree file
+     * @see #FILE_NAME_DEPENDENCY_TREE
+     */
     FileReference getDependencyTree();
 
+    /**
+     * Returns a reference to the configuration file of the UUF app represented by this reference.
+     *
+     * @return reference to the configuration file of this UUF app
+     * @see #FILE_NAME_CONFIGURATION
+     */
     FileReference getConfiguration();
 
+    /**
+     * Returns the absolute path to the UUF app represented by this reference.
+     *
+     * @return absolute path to the UUF app
+     */
     String getPath();
 }
