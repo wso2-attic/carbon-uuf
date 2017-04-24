58381e830a4abe80afdfd3506b84d392a71a6654
Sajith Janaprasad <sajithar@wso2.com>
2017-04-24 14:18:22 +0530

    fix javadoc in UUFException class

--- components/uuf-core/src/main/java/org/wso2/carbon/uuf/exception/UUFException.java
+++ components/uuf-core/src/main/java/org/wso2/carbon/uuf/exception/UUFException.java
@@ -18,19 +18,46 @@
 
 package org.wso2.carbon.uuf.exception;
 
+/**
+ * Indicates a generic exception occurred in UUF.
+ *
+ * @since 1.0.0
+ */
 public class UUFException extends RuntimeException {
 
+    /**
+     * Constructs a new exception with {@code null} as its detail message. The cause is not initialized, and may
+     * subsequently be initialized by a call to {@link #initCause(Throwable)}.
+     */
     public UUFException() {
     }
 
+    /**
+     * Constructs a new exception with the specified detail message. The cause is not initialized, and may
+     * subsequently be initialized by a call to {@link #initCause}.
+     *
+     * @param message the detail message of the exception
+     */
     public UUFException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new exception with the specified cause and a detail message of {@code (cause==null ? null :
+     * cause.toString())} which typically contains the class and detail message of the {@code cause}.
+     *
+     * @param cause the cause of the exception
+     */
     public UUFException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message the detail message of the exception
+     * @param cause   the cause of the exception
+     */
     public UUFException(String message, Throwable cause) {
         super(message, cause);
     }
