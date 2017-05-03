a8844d62de99263ab95e07edccc5a208c142230e
Sajith Janaprasad <sajithar@wso2.com>
2017-05-03 11:38:15 +0530

    add rest-apis page to features-app sample app

---
+++ samples/apps/org.wso2.carbon.uuf.sample.features-app.app/src/main/public/js/rest-apis.js
@@ -0,0 +1,37 @@
+/*<Export-Package>
+                            org.wso2.carbon.uuf.renderablecreator.html.*;version="${carbon.uuf.version}"
+                        </Export-Package>
+                        <Carbon-Component>
+                            osgi.service;objectClass="org.wso2.carbon.uuf.spi.RenderableCreator"
+                        </Carbon-Component>
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+$(function() {
+    $('#hello-btn').on('click', function (e) {
+        $.ajax({
+            url: "/features-app/root/apis/hello/" + $('#username').val(),
+            type: "GET",
+            success: function(result,status,xhr){
+                alert(result);
+            },
+            error: function(xhr,status,error){
+                alert(error);
+            }
+        });
+    })
+});
