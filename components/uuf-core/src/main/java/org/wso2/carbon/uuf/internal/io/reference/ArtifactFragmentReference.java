d9a259a78ba93ee8b4fb6345a64841ec6e5012a8
Sajith Janaprasad <sajithar@wso2.com>
2017-05-29 19:07:14 +0530

    remove usage of UUFException in ArtifactFragmentReference class

--- components/uuf-core/src/main/java/org/wso2/carbon/uuf/internal/io/reference/ArtifactFragmentReference.java
+++ components/uuf-core/src/main/java/org/wso2/carbon/uuf/internal/io/reference/ArtifactFragmentReference.java
@@ -18,10 +18,10 @@
 
 package org.wso2.carbon.uuf.internal.io.reference;
 
+import org.wso2.carbon.uuf.api.exception.UUFRuntimeException;
 import org.wso2.carbon.uuf.api.reference.ComponentReference;
 import org.wso2.carbon.uuf.api.reference.FileReference;
 import org.wso2.carbon.uuf.api.reference.FragmentReference;
-import org.wso2.carbon.uuf.exception.UUFException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -55,8 +55,8 @@ public class ArtifactFragmentReference implements FragmentReference {
                 return new ArtifactFileReference(renderingFilePath, componentReference.getAppReference());
             }
         }
-        throw new UUFException("Fragment '" + fragmentName + "' of component '" + componentReference.getPath() +
-                                       "' is empty.");
+        throw new UUFRuntimeException("Fragment '" + fragmentName + "' of component '" + componentReference.getPath() +
+                                              "' is empty.");
     }
 
     @Override
